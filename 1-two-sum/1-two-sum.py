767703842eb531e58853ba91598569e830d5a6ec
Alex Bowe <bowe.alexander@gmail.com>
2022-06-15 12:10:27 -0700

    Time: 74 ms (77.48%), Space: 15.2 MB (49.26%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i,x in enumerate(nums):
+            delta = target - x
+            if delta in d:
+                return [d[delta], i]
+            d[x] = i
+        
