6597c664b489c48b42f8a2fa73d0cfbd0572d899
Alex Bowe <bowe.alexander@gmail.com>
2022-07-04 16:49:09 -0700

    Time: 61 ms (70.58%), Space: 19.3 MB (8.61%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alphabet = set("abcdefghijklmnopqrstuvwxyz0123456789")
-        def filter_non_alpha(s):
-            for x in s:
-                if x.lower() in alphabet:
-                    yield x.lower()
-        s = "".join(filter_non_alpha(s))
+        s = "".join(x.lower() for x in s if x.isalnum())
         return s == s[::-1]
