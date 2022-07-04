5cb57a8fd982dd286d99b5713f138e18c7fa9aa0
Alex Bowe <bowe.alexander@gmail.com>
2022-07-04 16:59:53 -0700

    Time: 48 ms (49.16%), Space: 13.9 MB (24.00%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,12 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         parens = {x:y for x,y in "{} [] ()".split()}
+        
         stack = []
         for x in s:
-            if x in parens.keys():
+            if x in parens:
                 stack.append(parens[x])
             else:
-                if not stack: return False
+                if len(stack) == 0: return False
                 if stack.pop() != x: return False
         return len(stack) == 0
-                
