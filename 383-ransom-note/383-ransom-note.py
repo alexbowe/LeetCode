83f64715d03beb64e35cebfc30b03e3f269a4409
Alex Bowe <bowe.alexander@gmail.com>
2022-07-04 20:56:43 -0700

    Time: 64 ms (81.21%), Space: 14.1 MB (52.21%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,13 +1,4 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        from collections import defaultdict
-        counts = defaultdict(int)
-        for x in magazine:
-            counts[x]+=1
-        
-        for x in ransomNote:
-            if counts[x] == 0:
-                return False
-            counts[x]-=1
-        
-        return True
+        from collections import Counter
+        return not bool(Counter(ransomNote) - Counter(magazine))
