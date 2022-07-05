2db9cb6ea0c61089e82fa1373ea0e01495a52fb5
Alex Bowe <bowe.alexander@gmail.com>
2022-07-04 17:02:14 -0700

    Time: 928 ms (66.53%), Space: 27.9 MB (78.03%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,11 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        result = nums[0]
-        current = nums[0]
-        
-        for x in nums[1:]:
-            # if prefix sum is negative, dont add it to the current subarray
-            current = max(current + x, x)
-            result = max(result, current)
-        return result
-           
+        max_sum = nums[0]
+        curr_sum = nums[0]
+        for i in range(1, len(nums)):
+            curr_sum = max(nums[i], curr_sum+nums[i])
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
+            
