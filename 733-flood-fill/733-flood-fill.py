bd0dd0b65022264ce3fa64c7dae0324758b796ca
Alex Bowe <bowe.alexander@gmail.com>
2022-07-04 21:29:32 -0700

    Time: 131 ms (31.19%), Space: 14.1 MB (65.67%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,33 +1,23 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        """
-        Requirements
-        Examples
-        Solve
-        Pseudocode
-        Code
-        Test
+        height, width = len(image), len(image[0])
         
-        """
         original_color = image[sr][sc]
-        
         if original_color == color: return image
-
-        frontier = [(sr,sc)]
         
-        def bounds(image):
-            return len(image), len(image[0])
-        
-        def neighbors(row,col):
-            height, width = bounds(image)
-            if 0 <= row - 1:   yield (row-1, col)
-            if 0 <= col - 1:   yield (row  , col-1)
+        def neighbors(row, col):
+            if 0 <= row-1: yield (row-1,col)
+            if 0 <= col-1: yield (row, col-1)
             if row+1 < height: yield (row+1, col)
-            if col+1 < width:  yield (row  , col+1)
-
-        while frontier:
-            row, col = frontier.pop()
-            image[row][col] = color
-            frontier.extend((nr,nc) for (nr,nc) in neighbors(row,col) if image[nr][nc] == original_color)
+            if col+1 < width: yield (row, col+1)
         
+        stack = [(sr,sc)]
+        while stack:
+            row, col = stack.pop()
+            image[row][col] = color
+            
+            stack.extend(
+                (nr,nc) for (nr,nc) in neighbors(row,col)
+                if image[nr][nc] == original_color
+            )
         return image
