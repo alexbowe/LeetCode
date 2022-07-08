a1d54a5db197a53ad35def3fed9c40c5d53c12b0
Alex Bowe <bowe.alexander@gmail.com>
2022-07-08 00:46:50 -0700

    Time: 861 ms (81.90%), Space: 17.1 MB (92.70%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,27 +1,22 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:        
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         
-        results = []
-        for i in range(len(nums)-2):
-            x = nums[i]
-            if 0<i and x == nums[i-1]: continue
-            
-            target = -x
-            j, k = i+1, len(nums)-1
-            while j<k:
-                val = nums[j] + nums[k]
-                if val <= target:
-                    if val == target:
-                        results.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    while j<len(nums) and nums[j] == nums[j-1]: j+=1
-                else:
-                    k -= 1
-                    while 0<k and nums[k] == nums[k+1]: k-=1
-            
-        return results
-                
-            
-
+        def twoSum(nums, left, right, target):
+            deltas = dict()
+            for i in range(left, right):
+                x = nums[i]
+                delta = target - x
+                if delta in deltas:
+                    yield (deltas[delta],i)
+                deltas[x] = i
         
+        result = set()
+        for i in range(len(nums)-1):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            
+            # find two numbers in remainder of list that add up to give -nums[i]
+            target = -nums[i]
+            for j,k in twoSum(nums, i+1, len(nums), target):
+                result.add((nums[i], nums[j], nums[k]))
+        return list(result)
