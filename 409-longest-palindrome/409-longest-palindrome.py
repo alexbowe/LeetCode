47fcc60ac27b590a2d42fe14fd7c043d149197e5
Alex Bowe <bowe.alexander@gmail.com>
2022-07-07 19:46:44 -0700

    Time: 52 ms (46.77%), Space: 14 MB (21.91%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,12 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         from collections import Counter
-        c = Counter(s)
+        counts = Counter(s)
         
         doubles = 0
         singles = 0
-        for x in c:
-            doubles += c[x]//2
-            singles += c[x]%2
+        for x in counts:
+            doubles += counts[x]//2
+            singles += counts[x]%2
         
-        return doubles * 2 + (1 if singles > 0 else 0)
+        return 2*doubles + min(singles,1)
