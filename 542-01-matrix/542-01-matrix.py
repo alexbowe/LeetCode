37e2f8f99ee3a2f0402348d446e22a92262f4350
Alex Bowe <bowe.alexander@gmail.com>
2022-07-07 20:14:22 -0700

    Time: 625 ms (95.31%), Space: 16.6 MB (94.93%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,29 +1,27 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        height, width = len(mat), len(mat[0])
+        """
+        000
+        010
+        111
         
-        def neighbors(row,col):
-            if 0<=row-1: yield row-1,col
-            if 0<=col-1: yield row,col-1
-            if row+1<height: yield row+1,col
-            if col+1<width: yield row,col+1
+          012
+        0 000
+        1 010
+        2 111
+        """
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
+                if mat[row][col] == 0: continue
+                top = mat[row-1][col] if 0<=row-1 else math.inf
+                left = mat[row][col-1] if 0<=col-1 else math.inf
+                mat[row][col] = min(top+1, left+1)
         
-        level = []
-        seen = set()
+        for row in reversed(range(len(mat))):
+            for col in reversed(range(len(mat[0]))):
+                if mat[row][col] == 0: continue
+                bottom = mat[row+1][col] if row+1<len(mat) else math.inf
+                right = mat[row][col+1] if col+1<len(mat[0]) else math.inf
+                mat[row][col] = min([mat[row][col], bottom+1, right+1])
         
-        for row in range(height):
-            for col in range(width):
-                if mat[row][col] == 0:
-                    seen.add((row,col))
-                    level.append((row,col))
-        
-        steps = 0
-        while level:
-            for row, col in level:
-                mat[row][col] = steps
-
-            level = [n for x in level for n in neighbors(*x) if n not in seen]
-            seen.update(level)
-            steps += 1
-            
         return mat
