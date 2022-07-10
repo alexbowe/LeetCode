1fb9c074773e7777750711ea4403fb88a69a410f
Alex Bowe <bowe.alexander@gmail.com>
2022-07-09 20:38:17 -0700

    Time: 57 ms (38.74%), Space: 13.9 MB (72.63%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,21 +1,15 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        """
-        11
-        01
-        """
+        a, b = list(a), list(b)
+        
         carry = 0
         bits = []
-        
-        a = list(a)
-        b = list(b)
-        
-        while a or b or carry>0:
+        while a or b or carry:
             x = int(a.pop()) if a else 0
             y = int(b.pop()) if b else 0
-            total = x + y + carry
-            bit = total%2
-            carry = total//2
+            val = x+y+carry
+            bit = val%2
+            carry = val//2
             bits.append(str(bit))
         
         return "".join(bits[::-1])
