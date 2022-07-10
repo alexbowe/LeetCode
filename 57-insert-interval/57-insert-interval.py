87633bbfa60c8c465928f67d19a9c4ef76d27ccf
Alex Bowe <bowe.alexander@gmail.com>
2022-07-09 20:35:05 -0700

    Time: 149 ms (25.70%), Space: 17.7 MB (6.90%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,18 +1,19 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         from bisect import bisect_left
-        insert_idx = bisect_left(intervals, tuple(newInterval), key=tuple)
-        intervals.insert(insert_idx, newInterval)
+        i = bisect_left(intervals, tuple(newInterval), key=tuple)
+        intervals.insert(i, newInterval)
         
         def overlap(a,b):
             return b[0]<=a[1]
         
         def merge(a,b):
             if overlap(a,b):
-                return [ [min(a[0], b[0]), max(a[1], b[1])] ]
-            return [a, b]
+                return [[min(a[0],b[0]),max(a[1],b[1])]]
+            return [a,b]
         
         def reducer(xs, x):
-            return xs + merge(xs.pop(), x) if xs else [x]
+            prev = xs.pop()
+            return xs + merge(prev, x)
         
-        return reduce(reducer, intervals, [])
+        return reduce(reducer, intervals[1:], [intervals[0]])
