f4771ebec0a3d3049b91d7a5d4987cc540959611
Alex Bowe <bowe.alexander@gmail.com>
2022-07-11 16:03:43 -0700

    Time: 256 ms (8.36%), Space: 15.9 MB (66.36%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,33 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def make_graph(num_nodes, edges):
-            g = {i:{"in":set(),"out":set()} for i in range(num_nodes)}
+        def make_graph(n, edges):
+            g = {i:{"in":set(), "out":set()} for i in range(n)}
             for u,v in edges:
                 g[u]["out"].add(v)
                 g[v]["in"].add(u)
             return g
         
-        def find_start_point(g):
+        def find_indegree_0(g):
+            result = []
             for v,node in g.items():
-                if not node["in"]: return v
-                
-        def delete_node(g, u):
-            if u not in g: return
-            for v in g[u]["out"]:
-                g[v]["in"].remove(u)
-                
-            for v in g[u]["in"]:
-                g[v]["out"].remove(u)
-            
-            del g[u]
-            return g
-            
-        g = make_graph(numCourses, prerequisites)
-        while g:
-            i = find_start_point(g)
-            if i is None: return False
-            g = delete_node(g, i)
+                if not node["in"]:
+                    result.append(v)
+            return result
         
+        def del_node(g, v):
+            for i in g[v]["in"]:
+                g[i]["out"].remove(v)
+            for o in g[v]["out"]:
+                g[o]["in"].remove(v)
+            del g[v]
+            return g
+        
+        g = make_graph(numCourses, prerequisites)
+        
+        while g:
+            roots = find_indegree_0(g)
+            if not roots: return False
+            for v in roots:
+                del_node(g, v)
+                
         return True
             
-        
