a6ff2769bd0ee44f34bb65c5836227a90f761dc4
Alex Bowe <bowe.alexander@gmail.com>
2022-07-11 00:36:45 -0700

    Time: 1280 ms (40.59%), Space: 20.4 MB (37.38%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -3,14 +3,35 @@ class Solution:
         def d(x):
             return x[0]**2 + x[1]**2
         
-        import heapq
+        def choose_pivot(xs, left, right, k):
+            if right-left+1 < 3:
+                return right
+            mid = left + (right - left)//2
+            return sorted([left, mid, right], key=lambda i:k(xs[i]))[1]
         
-        heap = []
-        for x in points:
-            dist = d(x)
-            if len(heap) + 1 > k:
-                heapq.heappushpop(heap, (-dist, x))
-            else:
-                heapq.heappush(heap, (-dist, x))
+        def partition(xs, left, right, key=lambda x:x):
+            p = choose_pivot(xs, left, right, key)
+            pivot_value = key(xs[p])
+            xs[right], xs[p] = xs[p], xs[right]
+            
+            store = left
+            # swap pivot and store?
+            for i in range(left,right):
+                if key(xs[i]) <= pivot_value:
+                    xs[store], xs[i] = xs[i], xs[store]
+                    store+=1
+            xs[store], xs[right] = xs[right], xs[store]
+            return store
         
-        return [x for _,x in heap]
+        def quickselect(xs, k, key=lambda x:x):
+            left, right = 0, len(xs)-1
+            while left < right:
+                p = partition(xs, left, right, key)
+                if p == k-1: break
+                if p < k-1:
+                    left = p+1
+                if k-1 < p:
+                    right = p-1
+            return xs[:k]
+        
+        return quickselect(points, k, key=d)
