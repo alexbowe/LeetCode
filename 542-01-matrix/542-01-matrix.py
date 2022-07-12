d235edc287773a01d537fd1b7ce151da362552a8
Alex Bowe <bowe.alexander@gmail.com>
2022-07-11 17:20:54 -0700

    Time: 1084 ms (42.03%), Space: 16.9 MB (78.13%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,27 +1,20 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        """
-        000
-        010
-        111
+        height, width = len(mat), len(mat[0])
         
-          012
-        0 000
-        1 010
-        2 111
-        """
-        for row in range(len(mat)):
-            for col in range(len(mat[0])):
+        N = height*width
+        for row in range(height):
+            for col in range(width):
                 if mat[row][col] == 0: continue
-                top = mat[row-1][col] if 0<=row-1 else math.inf
-                left = mat[row][col-1] if 0<=col-1 else math.inf
-                mat[row][col] = min(top+1, left+1)
+                top = mat[row-1][col] if 0<=row-1 else N
+                left = mat[row][col-1] if 0<=col-1 else N
+                mat[row][col] = min(top, left) + 1
         
-        for row in reversed(range(len(mat))):
-            for col in reversed(range(len(mat[0]))):
+        for row in reversed(range(height)):
+            for col in reversed(range(width)):
                 if mat[row][col] == 0: continue
-                bottom = mat[row+1][col] if row+1<len(mat) else math.inf
-                right = mat[row][col+1] if col+1<len(mat[0]) else math.inf
-                mat[row][col] = min([mat[row][col], bottom+1, right+1])
+                bottom = mat[row+1][col] if row+1<height else N
+                right = mat[row][col+1] if col+1<width else N
+                mat[row][col] = min(mat[row][col], bottom+1, right+1)
         
         return mat
