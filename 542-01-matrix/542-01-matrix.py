1a98371f0d8750dc316cc552787be7cfbce81892
Alex Bowe <bowe.alexander@gmail.com>
2022-07-12 13:52:05 -0700

    Time: 723 ms (85.55%), Space: 16.9 MB (83.38%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,20 +1,20 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        height, width = len(mat), len(mat[0])
+        H, W = len(mat), len(mat[0])
+        N = H*W
         
-        N = height*width
-        for row in range(height):
-            for col in range(width):
-                if mat[row][col] == 0: continue
-                top = mat[row-1][col] if 0<=row-1 else N
-                left = mat[row][col-1] if 0<=col-1 else N
-                mat[row][col] = min(top, left) + 1
-        
-        for row in reversed(range(height)):
-            for col in reversed(range(width)):
-                if mat[row][col] == 0: continue
-                bottom = mat[row+1][col] if row+1<height else N
-                right = mat[row][col+1] if col+1<width else N
-                mat[row][col] = min(mat[row][col], bottom+1, right+1)
+        for r in range(H):
+            for c in range(W):
+                if mat[r][c] == 0: continue
+                up = mat[r-1][c] if 0<=r-1 else N
+                left = mat[r][c-1] if 0<=c-1 else N
+                mat[r][c] = min(up+1, left+1)
+                
+        for r in reversed(range(H)):
+            for c in reversed(range(W)):
+                if mat[r][c] == 0: continue
+                down = mat[r+1][c] if r+1<H else N
+                right = mat[r][c+1] if c+1<W else N
+                mat[r][c] = min(mat[r][c], down+1, right+1)
         
         return mat
