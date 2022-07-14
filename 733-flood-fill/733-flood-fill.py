547e4991a24e445c18e42f008dbb44f5148890ac
Alex Bowe <bowe.alexander@gmail.com>
2022-07-13 17:21:22 -0700

    Time: 103 ms (64.21%), Space: 14 MB (65.83%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,23 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        height, width = len(image), len(image[0])
+        H,W = len(image), len(image[0])
         
         original_color = image[sr][sc]
-        if original_color == color: return image
+        if color == original_color: return image
         
         def neighbors(row, col):
-            if 0 <= row-1: yield (row-1,col)
-            if 0 <= col-1: yield (row, col-1)
-            if row+1 < height: yield (row+1, col)
-            if col+1 < width: yield (row, col+1)
+            if 0<=row-1: yield (row-1, col)
+            if 0<=col-1: yield (row, col-1)
+            if row+1<H: yield (row+1, col)
+            if col+1<W: yield (row, col+1)
         
         stack = [(sr,sc)]
+        seen = set()
         while stack:
-            row, col = stack.pop()
+            row,col = stack.pop()
+            if image[row][col] != original_color: continue
             image[row][col] = color
-            
-            stack.extend(
-                (nr,nc) for (nr,nc) in neighbors(row,col)
-                if image[nr][nc] == original_color
-            )
+            ns = list(neighbors(row,col))
+            stack.extend(n for n in ns if n not in seen)
+            seen.update(ns)
+        
         return image
