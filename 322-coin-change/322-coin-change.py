64bc5ada09bd04543ae877804f1112a801660df9
Alex Bowe <bowe.alexander@gmail.com>
2022-07-14 22:31:35 -0700

    Time: 996 ms (94.66%), Space: 15 MB (31.44%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,31 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        """
-        Input:
-        - array of coin denominations (infinite number of each denomination)
-        - amount to add up to
-        
-        Output:
-        - Fewest number of coins
-        - -1 if no solution
-        
-        Fewest number => BFS? Dijkstra?
-        Distance is num hops, so BFS should be fine
-        Each coin denomination is the edge
-        
-        Loops wont be a problem because the amount will go over (and no negative coins)
-        """
-        
+        q = collections.deque([(0,0)])
         seen = set()
-        q = collections.deque([(0, 0)])
         while q:
-            num, total = q.popleft()
-            if total > amount: continue
-            if total == amount: return num
-            
+            n, val = q.popleft()
+            if val == amount: return n
             for c in coins:
-                if total+c in seen: continue
-                seen.add(total+c)
-                q.append((num+1,total+c))
-            
+                if val + c > amount: continue
+                if (val+c) in seen: continue
+                q.append((n+1, val+c))
+                seen.add(val+c)
         return -1
