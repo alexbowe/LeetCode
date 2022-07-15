10a7c62ad5ffecac0a7ea03b5121105b3348ddc6
Alex Bowe <bowe.alexander@gmail.com>
2022-07-14 23:05:10 -0700

    Time: 350 ms (6.43%), Space: 15.9 MB (66.47%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -7,28 +7,26 @@ class Solution:
                 g[v]["in"].add(u)
             return g
         
-        def find_indegree_0(g):
-            result = []
-            for v,node in g.items():
-                if not node["in"]:
-                    result.append(v)
-            return result
+        def find_indegree_zero(g):
+            if not g: return []
+            xs = []
+            for u in g.keys():
+                if not g[u]["in"]:
+                    xs.append(u)
+            return xs
         
         def del_node(g, v):
-            for i in g[v]["in"]:
-                g[i]["out"].remove(v)
-            for o in g[v]["out"]:
-                g[o]["in"].remove(v)
+            for n in g[v]["in"]:
+                g[n]["out"].remove(v)
+            for n in g[v]["out"]:
+                g[n]["in"].remove(v)
             del g[v]
             return g
         
         g = make_graph(numCourses, prerequisites)
-        
-        while g:
-            roots = find_indegree_0(g)
-            if not roots: return False
-            for v in roots:
+        starts = find_indegree_zero(g)
+        while starts:
+            for v in starts:
                 del_node(g, v)
-                
-        return True
-            
+            starts = find_indegree_zero(g)
+        return not g
