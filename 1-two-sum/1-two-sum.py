93052dcb2b8a92f03def25ab17d3ae8a1a800c70
Alex Bowe <bowe.alexander@gmail.com>
2022-07-15 17:34:48 -0700

    Time: 85 ms (74.08%), Space: 15.1 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        deltas = dict()
+        diffs = dict()
         for i,x in enumerate(nums):
-            delta = target - x
-            if delta in deltas:
-                return (deltas[delta], i)
-            deltas[x] = i
+            d = target - x
+            if d in diffs:
+                return (diffs[d], i)
+            diffs[x] = i
