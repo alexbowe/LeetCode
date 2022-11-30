ac749cb81894c62274771bfe4865f6935705130e
Alex Bowe <bowe.alexander@gmail.com>
2022-11-29 21:00:04 -0800

    Time: 1170 ms (69.64%), Space: 16.6 MB (97.50%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,22 +1,24 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         height, width = len(mat), len(mat[0])
-        N = height*width
         
+        # min paths going up/left
         for r in range(height):
             for c in range(width):
                 if mat[r][c] == 0: continue
-                up = mat[r-1][c] if r>0 else N
-                left = mat[r][c-1] if c>0 else N
+                # If there were no zeros before this point, we are infinitely far away
+                up = mat[r-1][c] if r>0 else float("inf")
+                left = mat[r][c-1] if c>0 else float("inf")
                 mat[r][c] = min(up, left) + 1
         
-        print(mat)
-        
+        # min paths going down/right
         for r in reversed(range(height)):
             for c in reversed(range(width)):
                 if mat[r][c] == 0: continue
-                down = mat[r+1][c] if r+1<height else N
-                right = mat[r][c+1] if c+1<width else N
+                # If there were no zeros before this point, we are infinitely far away
+                down = mat[r+1][c] if r+1<height else float("inf")
+                right = mat[r][c+1] if c+1<width else float("inf")
+                # Include previous calculation in min check
                 mat[r][c] = min(min(down, right) + 1, mat[r][c])
         
         return mat
