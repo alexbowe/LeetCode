d2e1bdb33b917a34417ad48ac8f295bd529da575
Alex Bowe <bowe.alexander@gmail.com>
2022-11-30 12:59:53 -0800

    Time: 4564 ms (18.38%), Space: 21.9 MB (9.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,39 +1,21 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        """
-        
-        - Brute force O(N^3) time O(1) space
-        
-        - Recursively while saving solutions:
-        - P(i,j) = s[i] == s[j] and P(i+1,j-1)
-        - P(i,i) = True
-        - P(i,i+1) = s[i] == s[i+1]
-        
-        5
-        abcde
-        abc
-        bcd
-        cde
-        
-        5-w+1
-        """
-        
+    def longestPalindrome(self, s: str) -> str:        
         table = [[False]*len(s) for _ in range(len(s))]
         
-        # Palindromes of order 1
+        # First Order Palindromes
         for i in range(len(s)):
             table[i][i] = True
         
-        # Palindromes of order 2
+        # Second Order Palindromes
         for i in range(1, len(s)):
             table[i-1][i] = s[i-1] == s[i]
         
-        # Higher orders
-        for i in reversed(range(len(s)-1)):
+        # Higher Order Palindromes
+        # Needs to be done bottom-to-top because
+        # Recurrence relation depends on rows below
+        for i in reversed(range(len(s)-1)): # skip bottom row
             for j in range(i+2, len(s)):
                 table[i][j] = s[i] == s[j] and table[i+1][j-1]
-        
-        #print(table)
         
         # Find longest palindrome
         a, b = 0, 0
