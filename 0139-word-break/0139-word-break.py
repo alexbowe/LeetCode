8878db22ce401b502bb5811f5310d338c087f34e
Alex Bowe <bowe.alexander@gmail.com>
2022-11-30 13:13:47 -0800

    Time: 71 ms (60.44%), Space: 14 MB (70.59%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,15 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not s: return True
+        
         words = sorted(wordDict, key=lambda x: -len(x))
-        stack = [s]
         seen = set()
+        stack = [s]
         while stack:
             curr = stack.pop()
             if curr == "": return True
-            for word in words:
-                if curr.startswith(word):
-                    new_word = curr[len(word):]
-                    if new_word in seen: continue
-                    seen.add(new_word)
-                    stack.append(new_word)
+            for w in words:
+                if not curr.startswith(w): continue
+                new_word = curr[len(w):]
+                if new_word in seen: continue
+                seen.add(new_word)
+                stack.append(new_word)
+        
         return False
