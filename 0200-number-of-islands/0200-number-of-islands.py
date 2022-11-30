8337a700a6ae8bdb5ec55b14a93f910b16a3bc3c
Alex Bowe <bowe.alexander@gmail.com>
2022-11-30 13:08:55 -0800

    Time: 659 ms (62.76%), Space: 26.8 MB (6.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,42 +1,42 @@
+from collections import defaultdict
+
 class UnionFind:
     def __init__(self):
-        from collections import defaultdict
         self._parent = dict()
         self._rank = defaultdict(int)
-    
+        
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
-        x,y = sorted([x,y], key=lambda xs:self._rank[xs])
+        x,y = sorted([x,y], key=lambda x: self._rank[x])
         self._parent[y] = x
-        if self._rank[x] == self._rank[y]: self._rank[x] += 1
+        self._rank[x] = self._rank[x] == self._rank[y]
     
     def find(self, x):
         if x not in self._parent: self._parent[x] = x
         if x == self._parent[x]: return x
         self._parent[x] = self.find(self._parent[x])
         return self._parent[x]
-
+    
     def count(self):
-        return sum(1 for x,px in self._parent.items() if x == px)
+        return sum(1 for x,px in self._parent.items() if x==px)
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        height, width = len(grid), len(grid[0])
+        H,W = len(grid), len(grid[0])
         
-        def neighbors(row, col):
-            if row-1 >= 0:     yield (row-1,col)
-            if col-1 >= 0:     yield (row,col-1)
-            if row+1 < height: yield (row+1, col)
-            if col+1 < width:  yield (row, col+1)
+        def neighbors(r,c):
+            if r-1>=0: yield (r-1, c)
+            if c-1>=0: yield (r, c-1)
+            if  r+1<H: yield (r+1, c)
+            if  c+1<W: yield (r, c+1)
         
         uf = UnionFind()
-        
-        for r in range(height):
-            for c in range(width):
+        for r in range(H):
+            for c in range(W):
                 if grid[r][c] == "0": continue
                 uf.find((r,c))
-                for nr,nc in neighbors(r,c):
+                for nr, nc in neighbors(r,c):
                     if grid[nr][nc] == "0": continue
                     uf.union((r,c), (nr,nc))
         
