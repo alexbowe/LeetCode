71345a8af2e7d03678c6055e3af7591457a72315
Alex Bowe <bowe.alexander@gmail.com>
2022-12-01 12:24:37 -0800

    Time: 5247 ms (15.99%), Space: 21.9 MB (14.20%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,26 +1,20 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:        
+    def longestPalindrome(self, s: str) -> str:
         table = [[False]*len(s) for _ in range(len(s))]
         
-        # First Order Palindromes
         for i in range(len(s)):
             table[i][i] = True
         
-        # Second Order Palindromes
-        for i in range(1, len(s)):
-            table[i-1][i] = s[i-1] == s[i]
+        for i in range(len(s)-1):
+            table[i][i+1] = s[i] == s[i+1]
         
-        # Higher Order Palindromes
-        # Needs to be done bottom-to-top because
-        # Recurrence relation depends on rows below
-        for i in reversed(range(len(s)-1)): # skip bottom row
+        for i in reversed(range(len(s)-1)):
             for j in range(i+2, len(s)):
                 table[i][j] = s[i] == s[j] and table[i+1][j-1]
-        
-        # Find longest palindrome
-        a, b = 0, 0
+                    
+        a,b = 0,0
         for i in range(len(s)):
             for j in range(i, len(s)):
-                if table[i][j] and j-i > b-a: a,b = i,j
+                if j-i > b-a and table[i][j]: a,b = i,j
         
         return s[a:b+1]
