13314fc6ac2b6e7db597ef62b90322b7a073a8df
Alex Bowe <bowe.alexander@gmail.com>
2022-12-01 13:21:42 -0800

    Time: 115 ms (40.56%), Space: 13.9 MB (92.12%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -12,19 +12,18 @@ class Solution:
             if  r+1<H: yield (r+1,c)
             if  c+1<W: yield (r,c+1)
         
-        fresh = {(r,c) for r,c in product(range(H), range(W)) if grid[r][c] == FRESH}
-        rotten = {(r,c) for r,c in product(range(H), range(W)) if grid[r][c] == ROTTEN}
-        minutes = 0
+        cells = lambda: product(range(H), range(W))
+        
+        fresh = {(i,j) for i,j in cells() if grid[i][j] == FRESH}
+        rotten = {(i,j) for i,j in cells() if grid[i][j] == ROTTEN}
+        ticks = 0
         while rotten and fresh:
-            new_rotten = {
-                (nr,nc) for r,c in rotten
-                for nr,nc in neighbors(r,c)
-                if (nr, nc) in fresh
+            ticks += 1
+            rotten = {
+                (nr,nc) for (r,c) in rotten
+                for (nr,nc) in neighbors(r,c)
+                if (nr,nc) in fresh
             }
-            fresh.difference_update(new_rotten)
-            rotten = new_rotten
-            minutes += 1
+            fresh.difference_update(rotten)
         
-        if fresh: return -1
-        return minutes
-        
+        return ticks if not fresh else -1
