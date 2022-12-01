667ac3368ce86302b451fd6b129234e60e174020
Alex Bowe <bowe.alexander@gmail.com>
2022-11-30 18:01:06 -0800

    Time: 70 ms (29.81%), Space: 13.8 MB (97.48%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,10 +1,6 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         c = collections.Counter(nums)
         nums[:] = []
         for color, count in sorted(c.items()):
             nums.extend([color]*count)
-        return nums
