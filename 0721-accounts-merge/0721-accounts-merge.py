a02025eac1e8ceeff5b51e70d0151ddb36eef9dc
Alex Bowe <bowe.alexander@gmail.com>
2022-12-01 12:43:11 -0800

    Time: 828 ms (14.02%), Space: 18.4 MB (60.82%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -5,18 +5,18 @@ class UnionFind:
         self._parent = dict()
         self._rank = defaultdict(int)
         
-    def find(self, x):
-        if x not in self._parent: self._parent[x] = x
-        if x == self._parent[x]: return x
-        self._parent[x] = self.find(self._parent[x])
-        return self._parent[x]
-    
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
         x,y = sorted([x,y], key=lambda x: self._rank[x])
         self._parent[y] = x
-        self._rank[x] += self._rank[x] == self._rank[y]
+        self._rank[x] += self._rank[y] == self._rank[x]
+        
+    def find(self, x):
+        if x not in self._parent: self._parent[x] = x
+        if x == self._parent[x]: return x
+        self._parent[x] = self.find(self._parent[x])
+        return self._parent[x]
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
@@ -24,21 +24,19 @@ class Solution:
         
         for name, *emails in accounts:
             uf.find(emails[0])
-            for i in range(1, len(emails)):
-                uf.union(emails[i-1], emails[i])
+            for email in emails[1:]:
+                uf.union(emails[0], email)
         
-        directory = defaultdict(lambda: {"name": "", "emails": set()})
+        mapping = defaultdict(set)
+        for _, *emails in accounts:
+            key = uf.find(emails[0])
+            mapping[key].update(emails)
+            
+        result = []
         for name, *emails in accounts:
             key = uf.find(emails[0])
-            entry = directory[key]
-            entry["name"] = name
-            entry["emails"].update(emails)
-        
-        result = []
-        for entry in directory.values():
-            result.append([entry["name"]] + sorted(entry["emails"]))
+            if key not in mapping: continue
+            result.append([name] + sorted(mapping[key]))
+            del mapping[key]
         
         return result
-        
-        
-            
