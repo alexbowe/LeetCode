12a4a13049be371d9973e9d7763e50eff5067b38
Alex Bowe <bowe.alexander@gmail.com>
2022-11-30 17:52:13 -0800

    Time: 578 ms (36.96%), Space: 18.6 MB (54.36%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+class UnionFind:
+    def __init__(self):        
+        self._parent = dict()
+        self._rank = defaultdict(int)
+        
+    def find(self, x):
+        if x not in self._parent: self._parent[x] = x
+        if x == self._parent[x]: return x
+        self._parent[x] = self.find(self._parent[x])
+        return self._parent[x]
+    
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        x,y = sorted([x,y], key=lambda x: self._rank[x])
+        self._parent[y] = x
+        self._rank[x] += self._rank[x] == self._rank[y]
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        """
+        - go through all accounts, add connections to UF
+        - go through all accounts again, find each email in UF to get canonical email (CE)
+        -- Use CE -> Name
+        """
+        uf = UnionFind()
+        
+        for name, *emails in accounts:
+            uf.find(emails[0])
+            for i in range(1, len(emails)):
+                uf.union(emails[i-1], emails[i])
+        
+        directory = defaultdict(lambda: {"name": "", "emails": set()})
+        for name, *emails in accounts:
+            key = uf.find(emails[0])
+            entry = directory[key]
+            entry["name"] = name
+            entry["emails"].update(emails)
+        
+        result = []
+        for entry in directory.values():
+            result.append([entry["name"]] + sorted(entry["emails"]))
+        
+        return result
+        
+        
+            
