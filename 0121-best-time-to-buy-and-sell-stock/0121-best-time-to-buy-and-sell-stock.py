d4d5eb7a92bbba1df0028e173e3557547f2757bf
Alex Bowe <bowe.alexander@gmail.com>
2022-12-01 13:15:51 -0800

    Time: 3419 ms (5.11%), Space: 25.2 MB (7.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        Price can go up temporarily - don't want to sell prematurely.
-        Likewise, we can weather the storm if we can sell higher later.
-        Need to find maximum sub-array of daily profits (and losses).
-        """
         current_profit = 0
         max_profit = 0
         for i in range(1, len(prices)):
             daily_profit = prices[i] - prices[i-1]
-            current_profit = max(current_profit+daily_profit, daily_profit)
-            max_profit = max(max_profit, current_profit)
+            current_profit = max(current_profit + daily_profit, daily_profit)
+            max_profit = max(current_profit, max_profit)
         return max_profit
