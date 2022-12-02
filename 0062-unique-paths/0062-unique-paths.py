896ecd1b1c7b263a42bed38a8bd19bf1c0e0f34e
Alex Bowe <bowe.alexander@gmail.com>
2022-12-01 18:44:09 -0800

    Time: 34 ms (90.70%), Space: 13.8 MB (74.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,3 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        """
+        - m + n - 2 = the total number of downs/rights (the required length of the path)
+        - n - 1 - choose which positions to move to right
+        """
         return math.comb(m + n - 2, n - 1)
