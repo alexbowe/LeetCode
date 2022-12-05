cbf4cbc1d4fafcd22032a140c185b295ba136658
Alex Bowe <bowe.alexander@gmail.com>
2022-12-04 22:35:55 -0800

    Time: 738 ms (17.74%), Space: 18.3 MB (61.12%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,40 +1,38 @@
-from collections import defaultdict
-
 class UnionFind:
-    def __init__(self):        
+    def __init__(self):
         self._parent = dict()
-        self._rank = defaultdict(int)
-        
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        x,y = sorted([x,y], key=lambda x: self._rank[x])
-        self._parent[y] = x
-        self._rank[x] += self._rank[y] == self._rank[x]
-        
+        self._rank = collections.defaultdict(int)
+    
     def find(self, x):
         if x not in self._parent: self._parent[x] = x
         if x == self._parent[x]: return x
         self._parent[x] = self.find(self._parent[x])
         return self._parent[x]
 
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        x,y = sorted([x,y], key=self._rank.__getitem__)
+        self._parent[y] = x
+        self._rank[x] += self._rank[x] == self._rank[y]
+
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         uf = UnionFind()
-        
-        for name, *emails in accounts:
+        for _, *emails in accounts:
             uf.find(emails[0])
-            for email in emails[1:]:
-                uf.union(emails[0], email)
+            for email in emails[1:]: uf.union(emails[0], email)
         
-        mapping = defaultdict(lambda: {"name": None, "emails": set()})
+        d = collections.defaultdict(set)
+        for _, *emails in accounts:
+            key = uf.find(emails[0])
+            d[key].update(emails)
+        
+        result = []
         for name, *emails in accounts:
             key = uf.find(emails[0])
-            mapping[key]["name"] = name
-            mapping[key]["emails"].update(emails)
-            
-        result = []
-        for entry in mapping.values():
-            result.append([entry["name"]] + sorted(entry["emails"]))
+            if key not in d: continue
+            result.append([name] + sorted(d[key]))
+            del d[key]
         
         return result
