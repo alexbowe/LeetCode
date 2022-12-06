ea357d71756f34472a0c48a9b53ec936c883ba6d
Alex Bowe <bowe.alexander@gmail.com>
2022-12-05 18:15:44 -0800

    Time: 161 ms (31.39%), Space: 13.9 MB (94.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = set()
-        q = [([], target)]
+        result = []
+        q = [([], candidates, target)]
         while q:
-            partial, target = q.pop()
-            if target < 0: continue
-            if target == 0: result.add(tuple(sorted(partial)))
-            for i in range(len(candidates)):
-                q.append((partial + [candidates[i]], target-candidates[i]))
+            partial, options, t = q.pop()
+            if t < 0: continue
+            if t == 0: result.append(partial)
+            for i in range(len(options)):
+                q.append((partial + [options[i]], options[i:], t-options[i]))
         return result
