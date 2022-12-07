3565ce70e312e1ad8787adb00e03f07d6dd1be56
Alex Bowe <bowe.alexander@gmail.com>
2022-12-07 13:02:40 -0800

    Time: 777 ms (98.12%), Space: 20.3 MB (82.36%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,5 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        from heapq import nsmallest
-        dist = lambda x: x[0]**2 + x[1]**2
-        return nsmallest(k, points, key=dist)
+        return heapq.nsmallest(k, points, key=lambda x: x[0]**2 + x[1]**2)
