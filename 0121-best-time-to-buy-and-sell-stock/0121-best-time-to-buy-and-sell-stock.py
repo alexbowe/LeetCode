fed29228807e5b74236e36218025a9fb3d7366b2
Alex Bowe <bowe.alexander@gmail.com>
2022-12-07 13:00:48 -0800

    Time: 2942 ms (20.66%), Space: 25 MB (39.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        current_profit = 0
-        max_profit = 0
-        for i in range(1, len(prices)):
+        current_max = 0
+        global_max = 0
+        for i in range(1,len(prices)):
             daily_profit = prices[i] - prices[i-1]
-            current_profit = max(current_profit + daily_profit, daily_profit)
-            max_profit = max(current_profit, max_profit)
-        return max_profit
+            current_max = max(current_max + daily_profit, daily_profit)
+            global_max = max(global_max, current_max)
+        return global_max
