363803998da0fc838ff2fdf3056379a915dabfef
Alex Bowe <bowe.alexander@gmail.com>
2022-12-07 12:57:45 -0800

    Time: 49 ms (81.44%), Space: 13.9 MB (97.89%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,7 +1,8 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def helper(partial, nums):
-            if not nums: yield partial
-            for i in range(len(nums)):
-                yield from helper(partial+[nums[i]], nums[:i] + nums[i+1:])
+        def helper(partial, remaining):
+            if not remaining: yield partial
+            for i in range(len(remaining)):
+                yield from helper(partial + [remaining[i]], remaining[:i] + remaining[i+1:])
         return list(helper([], nums))
+            
