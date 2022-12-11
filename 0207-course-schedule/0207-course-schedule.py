31d357f1d40c1db701d5880624d0afade7594bcc
Alex Bowe <bowe.alexander@gmail.com>
2022-12-10 21:20:44 -0800

    Time: 105 ms (89.46%), Space: 15.5 MB (76.27%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         def make_graph(prerequisites):
-            g = collections.defaultdict(lambda: {"incoming": set(), "outgoing": set()})
-            for u,v in prerequisites:
-                g[u]["incoming"].add(v)
-                g[v]["outgoing"].add(u)
-            return g    
-        
-        def find_indegree_zero(g):
-            return {u for u in g.keys() if not g[u]["incoming"]}
+            g = collections.defaultdict(lambda: {"out": set(), "indegree": 0})
+            for course,prereq in prerequisites:
+                g[course]["indegree"] += 1
+                g[prereq]["out"].add(course)
+            return g
         
         g = make_graph(prerequisites)
-        starts = find_indegree_zero(g)
-        while starts:
-            # delete all outgoing edges u,v
-            u = starts.pop()
-            while g[u]["outgoing"]:
-                v = g[u]["outgoing"].pop()
-                g[v]["incoming"].remove(u)
-                if not g[v]["incoming"] and not g[v]["outgoing"]: del g[v]
-                elif not g[v]["incoming"]: starts.add(v)
-            if not g[u]["incoming"] and not g[u]["outgoing"]: del g[u]
-        
-        print(g)
-        return len(g) == 0
+        roots = [v for v,node in g.items() if node["indegree"] == 0]
+        while roots:
+            c = roots.pop()
+            course = g[c]
+            for out in course["out"]:
+                g[out]["indegree"] -= 1
+                if g[out]["indegree"] == 0: roots.append(out)
+            del g[c]
+        return not bool(g)
+            
+            
