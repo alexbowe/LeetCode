3efa69d0ba5a2499e56ffb753fa230237a727b6d
Alex Bowe <bowe.alexander@gmail.com>
2022-12-11 13:54:56 -0800

    Time: 124 ms (27.54%), Space: 14 MB (48.43%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,7 +1,7 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        EMPTY  = 0
-        FRESH  = 1
+        EMPTY = 0
+        FRESH = 1
         ROTTEN = 2
         
         H,W = len(grid), len(grid[0])
@@ -11,12 +11,17 @@ class Solution:
             if c-1>=0: yield (r,c-1)
             if  r+1<H: yield (r+1,c)
             if  c+1<W: yield (r,c+1)
-                
-        fresh = {(r,c) for r,c in product(range(H),range(W)) if grid[r][c] == FRESH}
-        rotten = {(r,c) for r,c in product(range(H),range(W)) if grid[r][c] == ROTTEN}
-        ticks = 0
+        
+        fresh = {(r,c) for r,c in product(range(H), range(W)) if grid[r][c] == FRESH}
+        rotten = {(r,c) for r,c in product(range(H), range(W)) if grid[r][c] == ROTTEN}
+        
+        mins = 0
         while rotten and fresh:
-            ticks += 1
+            mins += 1
+            
             rotten = {(nr,nc) for r,c in rotten for nr,nc in neighbors(r,c) if (nr,nc) in fresh}
             fresh.difference_update(rotten)
-        return ticks if not fresh else -1
+        
+        return mins if not fresh else -1
+        
+            
