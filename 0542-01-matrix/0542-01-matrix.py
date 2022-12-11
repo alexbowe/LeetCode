254bf1f587421a7da0f0a4dd041990bc44e2ac1e
Alex Bowe <bowe.alexander@gmail.com>
2022-12-10 21:50:57 -0800

    Time: 571 ms (97.37%), Space: 16.9 MB (80.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,18 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        height, width = len(mat), len(mat[0])
+        H,W = len(mat), len(mat[0])
         
-        # min paths going up/left
-        for r in range(height):
-            for c in range(width):
-                if mat[r][c] == 0: continue
-                # If there were no zeros before this point, we are infinitely far away
-                up = mat[r-1][c] if r>0 else float("inf")
-                left = mat[r][c-1] if c>0 else float("inf")
-                mat[r][c] = min(up, left) + 1
+        for r,c in product(range(H), range(W)):
+            if mat[r][c] == 0: continue
+            up = mat[r-1][c] if r-1>=0 else float("inf")
+            left = mat[r][c-1] if c-1>=0 else float("inf")
+            mat[r][c] = min(up+1, left+1)
         
-        # min paths going down/right
-        for r in reversed(range(height)):
-            for c in reversed(range(width)):
+        for r in reversed(range(H)):
+            for c in reversed(range(W)):
                 if mat[r][c] == 0: continue
-                # If there were no zeros before this point, we are infinitely far away
-                down = mat[r+1][c] if r+1<height else float("inf")
-                right = mat[r][c+1] if c+1<width else float("inf")
-                # Include previous up-left-min in min check
-                mat[r][c] = min(min(down, right) + 1, mat[r][c])
+                down = mat[r+1][c] if r+1<H else float("inf")
+                right = mat[r][c+1] if c+1<W else float("inf")
+                mat[r][c] = min(mat[r][c], down+1, right+1)
         
         return mat
