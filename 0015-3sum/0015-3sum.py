dcf32f87a3e1c75c062c66147bd48dc44ad406a7
Alex Bowe <bowe.alexander@gmail.com>
2022-12-10 21:31:36 -0800

    Time: 8542 ms (5.00%), Space: 17 MB (95.42%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,18 +1,15 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         def twoSum(nums, start, target):
             d = dict()
             for i in range(start, len(nums)):
-                x = nums[i]
-                diff = target - x
-                if diff in d: yield (d[diff], i)
-                d[x] = i
+                delta = target - nums[i]
+                if delta in d: yield [d[delta], i]
+                d[nums[i]] = i
         
-        nums = sorted(nums)
-        
-        result = set()
+        results = set()
         for i in range(len(nums)-2):
             for j,k in twoSum(nums, i+1, -nums[i]):
-                result.add((nums[i], nums[j], nums[k]))
-        
-        return list(result)
+                results.add((nums[i], nums[j], nums[k]))
+        return list(results)
