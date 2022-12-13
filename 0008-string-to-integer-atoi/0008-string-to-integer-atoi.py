ca427580a577efce9dc245ab69b6ae42bdda1c25
Alex Bowe <bowe.alexander@gmail.com>
2022-12-13 13:08:28 -0800

    Time: 35 ms (94.86%), Space: 13.8 MB (99.09%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,10 +1,9 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         MIN, MAX = -2**31, 2**31 - 1
-        
         sign, value = 1, 0
         for i,x in enumerate(s.lstrip(" ")):
-            if i == 0 and x == "-": sign = -1
+            if   i == 0 and x == "-": sign = -1
             elif i == 0 and x == "+": sign = 1
             elif x.isdigit(): value = value*10 + int(x)
             else: break
