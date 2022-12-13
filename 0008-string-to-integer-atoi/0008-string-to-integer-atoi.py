4170b6c17be77c5fc34a3704b0113bf8d3a1133e
Alex Bowe <bowe.alexander@gmail.com>
2022-12-13 13:03:03 -0800

    Time: 39 ms (89.08%), Space: 13.9 MB (79.82%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -3,12 +3,8 @@ class Solution:
         MIN = -2**31
         MAX = 2**31 - 1
         
-        whitespace = " "
-        s = s.lstrip(whitespace)
-        
-        value = 0
-        sign = 1
-        for i,x in enumerate(s):
+        sign, value = 1, 0
+        for i,x in enumerate(s.lstrip(" ")):
             if i == 0 and x == "-": sign = -1
             elif i == 0 and x == "+": sign = 1
             elif x.isdigit(): value = value*10 + int(x)
