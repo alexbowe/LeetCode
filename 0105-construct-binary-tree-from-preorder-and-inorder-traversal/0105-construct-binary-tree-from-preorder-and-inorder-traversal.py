7a5f40629fa3a8a3083a8c85db57d48b15f3d530
Alex Bowe <bowe.alexander@gmail.com>
2022-12-13 17:49:55 -0800

    Time: 217 ms (61.04%), Space: 88.5 MB (41.35%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,10 +6,18 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        indices = {x:i for i,x in enumerate(inorder)}
+        """
+            x
+          [ 3, 9,20,15, 7]
+          [ 9, 3,15,20, 7]
+          
+        1. Next item of preorder is current root - always get value from here
+        2. Current root splits inorder in two
+        3. Arrays need same size: split preorder at same position after popping
+        """
         if not preorder or not inorder: return None
         node = TreeNode(preorder[0])
-        i = indices[preorder[0]]
-        node.left = self.buildTree(preorder[1:i+1],inorder[:i])
-        node.right = self.buildTree(preorder[i+1:],inorder[i+1:])
+        i = inorder.index(preorder[0])
+        node.left  = self.buildTree(preorder[1:i+1], inorder[:i])
+        node.right = self.buildTree(preorder[i+1:],  inorder[i+1:])
         return node
