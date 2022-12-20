ac8137405214307cad5bd1790d3138240b43cbfb
Alex Bowe <bowe.alexander@gmail.com>
2022-12-19 18:19:16 -0800

    Time: 1609 ms (65.51%), Space: 27.6 MB (94.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -3,6 +3,11 @@ class Solution:
         s = []
         max_area = 0
         for right,x in enumerate(heights+[0]):
+            # Find the biggest rectangle that ends at right (not including right)
+            # To do this, we pop all previous bars that are bigger than right
+            # and calculate the rectangle given by that bar up to right (not inclusive).
+            # These bars don't get used again - they will always be used in the largest
+            # rectangle possible to include them.
             while s and x <= heights[s[-1]]:
                 H = heights[s.pop()]
                 W = right if not s else right-s[-1]-1
