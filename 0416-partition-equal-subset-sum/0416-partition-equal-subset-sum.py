3ce332c45b7791a0f425da86c37ebdc07c671e36
Alex Bowe <bowe.alexander@gmail.com>
2022-12-21 15:41:43 -0800

    Time: 868 ms (84.99%), Space: 18.2 MB (46.47%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,16 +1,6 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        """
-         1 5 11 5
-        
-        How would I brute force it?
-        Try every partition and sum them up and compare
-        - Get total. Partition sum should be total/2. Total hence needs to be even.
-        - Find the subset of elements that add to give total/2
-        """
         total = sum(nums)
         if total%2 != 0: return False
-        sums = {0}
-        for n in nums:
-            sums |= {(s+n) for s in sums}
+        sums = reduce(lambda xs,x: xs | {s + x for s in xs}, nums, {0})
         return total//2 in sums
