9b3e6a1c03dd96ef2b83ab7d00550cf6f2fb38d8
Alex Bowe <bowe.alexander@gmail.com>
2022-12-22 13:38:51 -0800

    Time: 106 ms (81.66%), Space: 15.4 MB (75.80%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,43 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        """
+        BODMAS
+        2 - 1 + 2
+        
+        
+        - digit:
+        -- multiply and increment number
+        
+        - +:
+        -- update operator
+        --- need to know how to 
+        
+        - -
+        """
+        num = 0
+        result = 0
+        sign = 1
+        stack = []
+        for i,x in enumerate(s):
+            if x.isdigit():
+                num = num*10 + int(x)
+            
+            if x in "+-)" or i == len(s)-1:
+                result += sign*num
+                sign = 1
+                num = 0
+            
+            if   x == "+": sign =  1
+            elif x == "-": sign = -1
+            elif x == "(":
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+            elif x == ")":
+                sign = stack.pop()
+                prev_result = stack.pop()
+                result = prev_result + sign*result
+                sign = 1
+                num = 0
+        return result
