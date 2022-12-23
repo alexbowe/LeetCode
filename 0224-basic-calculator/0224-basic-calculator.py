94e691d94e5c300970b26931f116f94e0148d50c
Alex Bowe <bowe.alexander@gmail.com>
2022-12-23 12:02:32 -0800

    Time: 88 ms (87.52%), Space: 15.3 MB (75.83%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -4,23 +4,22 @@ class Solution:
         sign = 1
         num = 0
         result = 0
+        
         for i,x in enumerate(s):
-            if x.isdigit(): num = num*10+int(x)
-            if i==len(s)-1 or x in "+-)":
+            if x.isdigit():
+                num = num*10 + int(x)
+                
+            if x in "+-)" or i == len(s)-1:
                 result += sign*num
-                sign = 1
-                num = 0
+                sign, num = 1, 0
             
             if   x == "-": sign = -1
             elif x == "+": sign =  1
             elif x == "(":
-                stack.append(result)
-                stack.append(sign)
-                result = 0
-                sign = 1
+                stack.extend([result, sign])
+                result, sign = 0, 1
             elif x == ")":
-                sign = stack.pop()
-                prev_result = stack.pop()
-                result = prev_result + sign * result
+                sign, prev_result = stack.pop(), stack.pop()
+                result = prev_result + sign*result
                 sign = 1
         return result
