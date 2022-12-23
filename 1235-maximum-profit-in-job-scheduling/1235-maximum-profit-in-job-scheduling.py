4b30c46bbaed267dea141beda1314b946a98c3b6
Alex Bowe <bowe.alexander@gmail.com>
2022-12-23 14:42:57 -0800

    Time: 534 ms (97.56%), Space: 31.9 MB (36.73%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,10 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        N = len(profit)
+        start,end,profit = zip(*sorted(zip(startTime, endTime, profit)))
+        skip = {i:bisect.bisect_left(start,end[i]) for i in range(N)}
+        
+        dp = [0] * (N+1)
+        for i in reversed(range(N)):
+            dp[i] = max(dp[i+1], profit[i] + dp[skip[i]])
+        return dp[0]
