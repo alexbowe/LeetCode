09b8a3aaac4d2086ea1f9e5d4d7e245ab0b738f4
Alex Bowe <bowe.alexander@gmail.com>
2022-12-23 13:34:32 -0800

    Time: 1486 ms (82.36%), Space: 13.9 MB (52.97%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,13 +5,17 @@ class Solution:
         
         H,W = len(board), len(board[0])
         
+        board_chars = {board[r][c] for r,c in product(range(H),range(W))}
+        word_chars = set(word)
+        if word_chars - board_chars: return False
+        
         def search(pos, i=0, seen=None):
             r,c = pos
             seen = seen or set()
-            if i == len(word): return True
+            if i == len(word):             return True
             if r<0 or c<0 or r>=H or c>=W: return False
-            if word[i] != board[r][c]: return False
-            if (r,c) in seen: return False
+            if word[i] != board[r][c]:     return False
+            if (r,c) in seen:              return False
             seen.add((r,c))
             result = any([
                 search((r+1,c), i+1, seen),
@@ -22,4 +26,4 @@ class Solution:
             seen.remove((r,c))
             return result
         
-        return any(search((r,c)) for r,c in product(range(H),range(W)))
+        return any(search((r,c)) for r,c in product(range(H),range(W)) if board[r][c] == word[0])
