8a5ad35aea41b0509402f8f56f33f2c8c5eafd0e
Alex Bowe <bowe.alexander@gmail.com>
2022-12-22 16:33:24 -0800

    Time: 1515 ms (82.41%), Space: 14.1 MB (14.37%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,35 +4,25 @@ class Solution:
         if not board: return False
         
         H,W = len(board), len(board[0])
+        board_chars = {board[r][c] for r,c in product(range(H), range(W))}
+        word_chars  = set(word)
+        if word_chars - board_chars: return False
         
-        board_chars = set(board[r][c] for r,c in product(range(H), range(W)))
-        word_chars = set(word)
-        if len(word_chars - board_chars) != 0: return False
-        
-        def neighbors(r,c):
-            if r-1>=0: yield (r-1,c)
-            if c-1>=0: yield (r,c-1)
-            if  r+1<H: yield (r+1,c)
-            if  c+1<W: yield (r,c+1)
-        
-        positions = [(r,c) for r,c in product(range(H), range(W)) if board[r][c] == word[0]]
-        
-        def dfs(pos, i=0, visited=None):
-            visited = visited or set()
+        def search(pos, i=0, visited=None):            
             r,c = pos
             if i == len(word): return True
-            if any([r<0, c<0, r>=H, c>=W]): return False
+            if r<0 or c<0 or r>=H or c>=W: return False
+            visited = visited or set()
             if (r,c) in visited: return False
             if board[r][c] != word[i]: return False
             visited.add((r,c))
             result = any([
-                dfs((r+1,c), i+1, visited),
-                dfs((r,c+1), i+1, visited),
-                dfs((r-1,c), i+1, visited),
-                dfs((r,c-1), i+1, visited),
+                search((r+1,c),i+1,visited),
+                search((r,c+1),i+1,visited),
+                search((r-1,c),i+1,visited),
+                search((r,c-1),i+1,visited),
             ])
             visited.remove((r,c))
             return result
         
-        return any(dfs(pos) for pos in positions)
-        
+        return any(search((r,c)) for r,c in product(range(H), range(W)))
