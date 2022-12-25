1720eb54ead0ac7802847c88029b2d855fad6668
Alex Bowe <bowe.alexander@gmail.com>
2022-12-24 19:11:02 -0800

    Time: 30 ms (97.10%), Space: 13.8 MB (97.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,5 +1,7 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         c = collections.Counter(nums)
-        nums[:] = [0] * c[0] + [1] * c[1] + [2] * c[2]
-        
+        nums[:] = [0]*c[0] + [1]*c[1] + [2]*c[2]
