506871be49518f5615ef61035e658297911f1531
Alex Bowe <bowe.alexander@gmail.com>
2022-12-24 19:05:20 -0800

    Time: 917 ms (85.29%), Space: 75.7 MB (48.18%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -4,19 +4,21 @@ class ListNode:
         self.val = val
         self.prev = None
         self.next = None
-    
-    def disconnect(self):
+        
+    def remove(self):
         if self.prev: self.prev.next = self.next
         if self.next: self.next.prev = self.prev
-        self.prev = None
         self.next = None
-    
+        self.prev = None
+        return self
+        
     def postinsert(self, x):
         x.prev = self
         x.next = self.next
         if self.next: self.next.prev = x
         self.next = x
-
+        return self
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -28,25 +30,20 @@ class LRUCache:
 
     def get(self, key: int) -> int:
         if key not in self._d: return -1
-        node = self._d[key]
-        node.disconnect()
+        node = self._d[key].remove()
         self._list_head.postinsert(node)
         return node.val
-    
-    def _evict(self):
-        if not self._d: return
-        node = self._list_tail.prev
-        node.disconnect()
-        del self._d[node.key]
 
     def put(self, key: int, value: int) -> None:
-        if self.get(key) != -1:
-            self._d[key].val = value
-            return
-        elif len(self._d) == self._capacity: self._evict()
-        self._d[key] = ListNode(key=key, val=value)
+        if self.get(key) != -1: self._d[key].val = value; return
+        
+        if len(self._d) == self._capacity:
+            k = self._list_tail.prev.remove().key
+            del self._d[k]
+            
+        self._d[key] = ListNode(key,value)
         self._list_head.postinsert(self._d[key])
-
+            
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
