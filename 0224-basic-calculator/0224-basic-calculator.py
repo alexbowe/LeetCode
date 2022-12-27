3a3ec901c8ad2ed44b554c00e53f16a48cea13a1
Alex Bowe <bowe.alexander@gmail.com>
2022-12-27 11:37:51 -0800

    Time: 104 ms (81.63%), Space: 15.4 MB (75.97%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,20 +1,21 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        result, num, sign = 0, 0, 1
         stack = []
-        result, num, sign = 0,0,1
         for i,x in enumerate(s):
             if x.isdigit(): num=num*10+int(x)
             
-            if i==len(s)-1 or x in "+-)":
+            if i==len(s)-1 or x in "-+)":
                 result += sign*num
-                num, sign = 0, 1
+                sign, num = 1, 0
             
-            if   x == "-": sign = -1
-            elif x == "+": sign =  1
+            if   x == "+": sign =  1
+            elif x == "-": sign = -1
             elif x == "(":
-                stack.extend([result,sign])
+                stack.extend([result, sign])
                 result, sign = 0, 1
             elif x == ")":
                 sign, prev_result = stack.pop(), stack.pop()
-                result = prev_result + sign * result
+                result = prev_result + sign*result
+                sign = 1
         return result
