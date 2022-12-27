541ca994c95da646516740b1249306a9c5e1db5c
Alex Bowe <bowe.alexander@gmail.com>
2022-12-27 11:46:44 -0800

    Time: 9262 ms (6.98%), Space: 13.9 MB (93.39%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,20 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not word: return True
-        if not board: return False
-        
         H,W = len(board), len(board[0])
-        
-        board_chars = {board[r][c] for r,c in product(range(H),range(W))}
-        word_chars = set(word)
-        if word_chars-board_chars: return False
-        
         def search(pos, i=0, seen=None):
             r,c = pos
-            if i == len(word): return True
+            seen = seen or set()
+            if i>=len(word):               return True
             if r<0 or c<0 or r>=H or c>=W: return False
-            if (r,c) in (seen:=seen or set()): return False
-            if board[r][c] != word[i]: return False
-            seen.add((r,c))
+            if pos in seen:                return False
+            if board[r][c] != word[i]:     return False
+            seen.add(pos)
             result = any([
-                search((r+1,c),i+1,seen),
-                search((r,c+1),i+1,seen),
-                search((r-1,c),i+1,seen),
-                search((r,c-1),i+1,seen),
+                search((r+1,c), i+1, seen),
+                search((r,c+1), i+1, seen),
+                search((r-1,c), i+1, seen),
+                search((r,c-1), i+1, seen),
             ])
-            seen.remove((r,c))
+            seen.remove(pos)
             return result
-        
-        return any(search((r,c)) for r,c in product(range(H), range(W)))
+        return any(search((r,c)) for r,c in product(range(H),range(W)))
