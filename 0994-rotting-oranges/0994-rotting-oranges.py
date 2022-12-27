c8dcdda7c0ea3427d90841f2400a7a5792ff0802
Alex Bowe <bowe.alexander@gmail.com>
2022-12-27 11:56:09 -0800

    Time: 60 ms (78.14%), Space: 13.8 MB (92.29%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,27 +1,22 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        EMPTY = 0
-        FRESH = 1
+        EMPTY  = 0
+        FRESH  = 1
         ROTTEN = 2
         
         H,W = len(grid), len(grid[0])
+        fresh  = {(r,c) for (r,c) in product(range(H), range(W)) if grid[r][c] == FRESH}
+        rotten = {(r,c) for (r,c) in product(range(H), range(W)) if grid[r][c] == ROTTEN}
         
         def neighbors(r,c):
             if r-1>=0: yield (r-1,c)
             if c-1>=0: yield (r,c-1)
-            if  r+1<H: yield (r+1,c)
-            if  c+1<W: yield (r,c+1)
+            if r+1<=H: yield (r+1,c)
+            if c+1<=W: yield (r,c+1)
         
-        fresh = {(r,c) for r,c in product(range(H), range(W)) if grid[r][c] == FRESH}
-        rotten = {(r,c) for r,c in product(range(H), range(W)) if grid[r][c] == ROTTEN}
-        
-        mins = 0
+        tick = 0
         while rotten and fresh:
-            mins += 1
-            
-            rotten = {(nr,nc) for r,c in rotten for nr,nc in neighbors(r,c) if (nr,nc) in fresh}
-            fresh.difference_update(rotten)
-        
-        return mins if not fresh else -1
-        
-            
+            tick+=1
+            rotten = {(nr,nc) for (r,c) in rotten for (nr,nc) in neighbors(r,c) if (nr,nc) in fresh}
+            fresh -= rotten
+        return tick if not fresh else -1
