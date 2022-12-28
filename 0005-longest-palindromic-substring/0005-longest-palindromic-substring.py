2d7c5050e2da25014e0f6cf529f9c5c77fc577c0
Alex Bowe <bowe.alexander@gmail.com>
2022-12-28 13:55:16 -0800

    Time: 4537 ms (20.35%), Space: 21.8 MB (15.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,15 +5,14 @@ class Solution:
         
         for i in range(N):
             P[i][i] = True
-            if i+1 < N: P[i][i+1] = s[i] == s[i+1]
+            if i+1<N: P[i][i+1] = s[i] == s[i+1]
         
         for i in reversed(range(N)):
-            for j in range(i+2,N):
+            for j in range(i+2, N):
                 P[i][j] = s[i] == s[j] and P[i+1][j-1]
         
         a,b = 0,0
         for i in range(N):
             for j in range(i,N):
-                if j-i > b-a and P[i][j]: a,b = i,j
-        
+                if P[i][j] and j-i>=b-a: a,b=i,j
         return s[a:b+1]
