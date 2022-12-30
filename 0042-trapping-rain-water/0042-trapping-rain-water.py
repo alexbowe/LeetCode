0d14e2578a6af424bad108db158bd27403188da7
Alex Bowe <bowe.alexander@gmail.com>
2022-12-30 12:51:55 -0800

    Time: 3517 ms (5.06%), Space: 16.4 MB (5.57%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,5 +1,5 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         left  = reduce(lambda xs,x: xs+[max(xs[-1],x)], height, [0])[1:]
-        right = reduce(lambda xs,x: xs+[max(xs[-1],x)], reversed(height), [0])[1:][::-1]
+        right = reduce(lambda xs,x: xs+[max(xs[-1],x)], height[::-1], [0])[1:][::-1]
         return sum(min(l,r)-h for l,r,h in zip(left,right,height))
