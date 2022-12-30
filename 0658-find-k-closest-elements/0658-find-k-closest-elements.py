c7ec6166d108383c86b6c8c15150082cce2fb80f
Alex Bowe <bowe.alexander@gmail.com>
2022-12-30 13:52:46 -0800

    Time: 427 ms (65.60%), Space: 16.7 MB (5.53%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        def key(value):
+            return abs(x-value), value
+        return sorted(heapq.nsmallest(k, arr, key=key))
