2fdeec54094673f9262fad1de5f098fd58717990
Alex Bowe <bowe.alexander@gmail.com>
2023-01-01 15:30:52 -0800

    Time: 139 ms (86.59%), Space: 15.2 MB (78.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,3 +1,3 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return reduce(int.__xor__, itertools.chain(range(len(nums)+1),nums))
+        return reduce(int.__xor__, nums)^reduce(int.__xor__, range(len(nums)+1))
