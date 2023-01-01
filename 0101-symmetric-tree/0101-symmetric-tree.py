3769cfee4cecde025111811ee53c932eacfca2a3
Alex Bowe <bowe.alexander@gmail.com>
2023-01-01 14:53:53 -0800

    Time: 35 ms (88.25%), Space: 13.9 MB (94.32%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,9 +6,7 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def helper(left, right):
-            if not left or not right: return bool(left)==bool(right)
-            if left.val!=right.val: return False
-            return helper(left.left, right.right) and helper(left.right, right.left)
-        
+        def helper(left,right):
+            if not left or not right: return bool(left) == bool(right)
+            return left.val == right.val and helper(left.left, right.right) and helper(left.right, right.left)
         return helper(root.left, root.right)
