ffaa7058a5add77fe187f3b8d78b9ba711b34d9f
Alex Bowe <bowe.alexander@gmail.com>
2022-12-31 16:37:47 -0800

    Time: 164 ms (96.05%), Space: 15.6 MB (63.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        N = len(nums)
+        nums[:] = (x for x in nums if x != 0)
+        n_zeros = N-len(nums)
+        nums.extend([0]*n_zeros)
