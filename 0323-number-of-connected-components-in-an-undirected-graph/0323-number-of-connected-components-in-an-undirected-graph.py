e1c0c6cc9529aef42ad69d2915962fc6d1712c85
Alex Bowe <bowe.alexander@gmail.com>
2023-01-01 15:26:03 -0800

    Time: 138 ms (63.83%), Space: 16 MB (47.43%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -12,17 +12,16 @@ class UnionFind:
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
-        x,y = sorted([x,y],key=self._rank.__getitem__)
+        x,y = sorted([x,y], key=self._rank.__getitem__)
         self._parent[y] = x
         self._rank[x] += self._rank[x] == self._rank[y]
     
     def count(self):
         return sum(x==p for x,p in self._parent.items())
-
+    
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         uf = UnionFind()
         for v in range(n): uf.find(v)
-        for u,v in edges:  uf.union(u,v)
+        for u,v in edges: uf.union(u,v)
         return uf.count()
-    
