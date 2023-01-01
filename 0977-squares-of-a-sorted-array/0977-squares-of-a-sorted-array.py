5f95240c5068169eb7a1875c4c9094cdb200f2e2
Alex Bowe <bowe.alexander@gmail.com>
2022-12-31 17:29:43 -0800

    Time: 263 ms (69.58%), Space: 15.9 MB (87.23%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        left = [x**2 for x in nums if x < 0]
+        right = [x**2 for x in nums if x >= 0]
+        return heapq.merge(right, reversed(left))
