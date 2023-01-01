fd8a6ee3cc598633648ce5f2e12986d871865510
Alex Bowe <bowe.alexander@gmail.com>
2022-12-31 17:22:13 -0800

    Time: 34 ms (86.96%), Space: 13.8 MB (48.65%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        n = (n>>16) | (n<<16)
+        n = ((n&0xFF00FF00)>>8) | ((n&0x00FF00FF)<<8)
+        n = ((n&0xF0F0F0F0)>>4) | ((n&0x0F0F0F0F)<<4)
+        n = ((n&0xCCCCCCCC)>>2) | ((n&0x33333333)<<2)
+        n = ((n&0xAAAAAAAA)>>1) | ((n&0x55555555)<<1)
+        return n
