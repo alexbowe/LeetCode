31547c6dd52ccd79e83af197422a9a9b842c6532
Alex Bowe <bowe.alexander@gmail.com>
2023-01-02 12:25:37 -0800

    Time: 532 ms (85.14%), Space: 25.7 MB (36.76%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        def make_graph(n, edges):
+        def make_graph(n,edges):
             g = {i:set() for i in range(n)}
             for u,v in edges:
                 g[u].add(v)
@@ -8,15 +8,13 @@ class Solution:
             return g
         
         g = make_graph(n, edges)
-        leaves = [v for v,neighbors in g.items() if len(neighbors)<=1]
-        
+        leaves = [v for v in g if len(g[v])<=1]
         while len(g)>2:
             new_leaves = []
             for v in leaves:
                 u = g[v].pop()
-                g[u].remove(v)
                 del g[v]
-                if len(g[u]) == 1: new_leaves.append(u)
+                g[u].remove(v)
+                if len(g[u])==1: new_leaves.append(u)
             leaves = new_leaves
-        
         return leaves
