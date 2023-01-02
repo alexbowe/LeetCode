65619f5224b6be9abf758beff65cc96af7f9f9ac
Alex Bowe <bowe.alexander@gmail.com>
2023-01-02 12:42:43 -0800

    Time: 570 ms (95.74%), Space: 17 MB (68.43%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,18 +1,16 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         H,W = len(mat), len(mat[0])
-        
-        for r,c in product(range(H), range(W)):
+        for r,c in product(range(H),range(W)):
             if mat[r][c] == 0: continue
             up = mat[r-1][c] if r-1>=0 else float("inf")
             left = mat[r][c-1] if c-1>=0 else float("inf")
             mat[r][c] = min(up+1, left+1)
         
-        for r in reversed(range(H)):
-            for c in reversed(range(W)):
-                if mat[r][c] == 0: continue
-                down = mat[r+1][c] if r+1<H else float("inf")
-                right = mat[r][c+1] if c+1<W else float("inf")
-                mat[r][c] = min(mat[r][c], down+1, right+1)
+        for r,c in reversed(list(product(range(H),range(W)))):
+            if mat[r][c] == 0: continue
+            down = mat[r+1][c] if r+1<H else float("inf")
+            right = mat[r][c+1] if c+1<W else float("inf")
+            mat[r][c] = min(down+1, right+1, mat[r][c])
         
         return mat
