fe0187d0bdcbabb55ecb05fbc3a7f49d8afe3b02
Alex Bowe <bowe.alexander@gmail.com>
2023-01-03 12:42:11 -0800

    Time: 133 ms (86.47%), Space: 14.7 MB (82.65%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,13 +3,13 @@ class Solution:
         need = collections.Counter(t)
         missing = len(t)
         
-        l, L, R = 0, 0, 0
-        for r,c in enumerate(s, 1):
+        l,L,R = 0,0,0
+        for r,c in enumerate(s,1):
             missing -= need[c]>0
             need[c] -= 1
-            if missing > 0: continue
+            if missing>0: continue
             while need[s[l]]<0:
-                need[s[l]] += 1
-                l += 1
-            if R==0 or r-l<=R-L: L,R=l,r
+                need[s[l]]+=1
+                l+=1
+            if R==0 or r-l<R-L: L,R=l,r
         return s[L:R]
