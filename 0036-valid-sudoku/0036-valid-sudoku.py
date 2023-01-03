1cbb21e4b16c8f09f69dd9c559a24a6cc6a38585
Alex Bowe <bowe.alexander@gmail.com>
2023-01-03 12:58:28 -0800

    Time: 320 ms (5.13%), Space: 14.1 MB (5.99%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        W = 9
-        SW = 3
-        
-        def row(r): return [board[r][c] for c in range(W)]
-        def col(c): return [board[r][c] for r in range(W)]
+        W,SW = 9,3
+        def row(i): return [board[i][c] for c in range(W)]
+        def col(i): return [board[r][i] for r in range(W)]
         def sqr(r,c): return [board[r*SW+sr][c*SW+sc] for sr,sc in product(range(SW),range(SW))]
         
         def is_valid(xs):
             c = collections.Counter(xs)
             del c["."]
-            return not c or max(c.values()) == 1
+            return not c or max(c.values())==1
         
-        rows = all(is_valid(row(r)) for r in range(W))
-        cols = all(is_valid(col(c)) for c in range(W))
-        sqrs = all(is_valid(sqr(r,c)) for r,c in product(range(SW),range(SW)))
+        return all([
+            all(is_valid(row(i)) for i in range(W)),
+            all(is_valid(col(i)) for i in range(W)),
+            all(is_valid(sqr(r,c)) for r,c in product(range(SW),range(SW)))
+        ])
+                
         
-        return rows and cols and sqrs
