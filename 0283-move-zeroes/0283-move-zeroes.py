115d79f5ac448239496b0570e7b3d1c83c6e3f97
Alex Bowe <bowe.alexander@gmail.com>
2023-01-04 12:52:28 -0800

    Time: 431 ms (40.46%), Space: 15.6 MB (15.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,5 +1,8 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         N = len(nums)
         nums[:] = (x for x in nums if x!=0)
-        nums.extend([0]*(N-len(nums)))
+        nums.extend(0 for _ in range(N-len(nums)))
