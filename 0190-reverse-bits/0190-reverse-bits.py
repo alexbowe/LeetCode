2ca6928a309ce35c72919fb94df2c4059d639109
Alex Bowe <alex@alexbowe.com>
2023-01-05 12:56:13 -0800

    Time: 42 ms (61.45%), Space: 13.9 MB (7.04%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -5,4 +5,5 @@ class Solution:
         n = ((n&0xF0F0F0F0)>>4)|((n&0x0F0F0F0F)<<4)
         n = ((n&0xCCCCCCCC)>>2)|((n&0x33333333)<<2)
         n = ((n&0xAAAAAAAA)>>1)|((n&0x55555555)<<1)
+        
         return n
