c7bdab7a7b177b983594976742a55d6aa02ad430
Alex Bowe <alex@alexbowe.com>
2023-01-07 12:47:15 -0800

    Time: 29 ms (97.25%), Space: 13.9 MB (99.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,3 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return reduce(lambda xs,x: xs+[r+[x] for r in xs], nums, [[]])
+        result = [[]]
+        for x in nums:
+            result += [r+[x] for r in result]
+        return result
