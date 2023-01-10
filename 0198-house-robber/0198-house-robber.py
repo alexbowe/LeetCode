c7045e07079453370bf80227240f35a4636e1886
Alex Bowe <alex@alexbowe.com>
2023-01-09 23:40:52 -0800

    Time: 35 ms (66.93%), Space: 13.9 MB (59.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,6 +2,5 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         a, b = 0, 0
         for x in nums:
-            # skip b and rob this, or rob b as well
-            a, b = b, max(a + x, b)          
+            a, b = b, max(a+x, b)
         return b
