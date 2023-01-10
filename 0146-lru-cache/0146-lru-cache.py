7821b64a45019eb95f528ab349d0a6fe8cffac01
Alex Bowe <alex@alexbowe.com>
2023-01-10 00:35:24 -0800

    Time: 935 ms (74.26%), Space: 75.6 MB (56.43%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,43 +2,42 @@ class ListNode:
     def __init__(self, key=None, val=None):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
         
     def remove(self):
         if self.prev: self.prev.next = self.next
         if self.next: self.next.prev = self.prev
-        self.next = None
         self.prev = None
+        self.next = None
         return self
-        
-    def postinsert(self, x):
-        x.prev = self
-        x.next = self.next
-        if self.next: self.next.prev = x
-        self.next = x
+    
+    def postinsert(self, other):
+        other.prev = self
+        other.next = self.next
+        if self.next: self.next.prev = other
+        self.next = other
         return self
-        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self._capacity = capacity
         self._d = dict()
-        self._list_head = ListNode()
-        self._list_tail = ListNode()
-        self._list_head.postinsert(self._list_tail)
+        self._head = ListNode()
+        self._tail = ListNode()
+        self._head.postinsert(self._tail)
 
     def get(self, key: int) -> int:
         if key not in self._d: return -1
-        node = self._d[key].remove()
-        self._list_head.postinsert(node)
-        return node.val
+        return self._head.postinsert(self._d[key].remove()).next.val
 
     def put(self, key: int, value: int) -> None:
         if self.get(key) != -1: self._d[key].val = value; return
-        if len(self._d) == self._capacity: del self._d[self._list_tail.prev.remove().key]
-        self._d[key] = self._list_head.postinsert(ListNode(key,value)).next
+        if len(self._d) == self._capacity: del self._d[self._tail.prev.remove().key]
+        self._d[key] = self._head.postinsert(ListNode(key,value)).next
             
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
