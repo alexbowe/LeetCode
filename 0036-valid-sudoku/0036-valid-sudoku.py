5c4fe10e4e506e18aec4946d84e6467cf7892849
Alex Bowe <alex@alexbowe.com>
2023-01-10 18:07:02 -0800

    Time: 126 ms (61.74%), Space: 13.8 MB (99.30%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        W,SW = 9,3
-        def row(i): return [board[i][c] for c in range(W)]
-        def col(i): return [board[r][i] for r in range(W)]
-        def sqr(r,c): return [board[r*SW+sr][c*SW+sc] for sr,sc in product(range(SW),range(SW))]
+        W = 9
+        SW = 3
+        row = lambda r: [board[r][c] for c in range(W)]
+        col = lambda c: [board[r][c] for r in range(W)]
+        sqr = lambda r,c: [board[r*SW+sr][c*SW+sc] for sr,sc in product(range(SW),range(SW))]
         
         def is_valid(xs):
             c = collections.Counter(xs)
             del c["."]
-            return not c or max(c.values())==1
+            return not c or max(c.values()) == 1
         
-        return all([
-            all(is_valid(row(i)) for i in range(W)),
-            all(is_valid(col(i)) for i in range(W)),
-            all(is_valid(sqr(r,c)) for r,c in product(range(SW),range(SW)))
-        ])
-                
-        
+        valid_rows = all(is_valid(row(r)) for r in range(W))
+        valid_cols = all(is_valid(col(c)) for c in range(W))
+        valid_sqrs = all(is_valid(sqr(r,c)) for r,c in product(range(SW),range(SW)))
+        return valid_rows and valid_cols and valid_sqrs
