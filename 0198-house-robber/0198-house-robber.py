5a95dc092f123e629064e889d74c32d8eff77fd5
Alex Bowe <alex@alexbowe.com>
2023-01-10 18:01:53 -0800

    Time: 36 ms (60.99%), Space: 13.8 MB (96.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,5 +2,5 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         a, b = 0, 0
         for x in nums:
-            a, b = b, max(a+x, b)
+            a, b = b, max(a+x,b)
         return b
