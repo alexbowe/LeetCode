1f3571127b2e4c6f2e42eb197fc657c7455a386d
Alex Bowe <alex@alexbowe.com>
2023-01-15 13:59:53 -0800

    Time: 95 ms (49.96%), Space: 17.5 MB (17.89%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,4 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort(key=tuple)
-        return not any(intervals[i][0]<intervals[i-1][1] for i in range(1,len(intervals)))
+        
+        overlap = lambda a,b: b[0]<a[1]
+        
+        def pairs(xs):
+            a,b = itertools.tee(xs)
+            next(b,None)
+            return zip(a,b)
+        
+        return not any(overlap(a,b) for a,b in pairs(intervals))
