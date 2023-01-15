322e16d0d3442bb0b2a06b06175d13a0ff3981f1
Alex Bowe <alex@alexbowe.com>
2023-01-14 21:47:36 -0800

    Time: 366 ms (99.89%), Space: 14.2 MB (87.56%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -2,5 +2,5 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         c = collections.Counter(tasks)
         f_max = max(c.values())
-        n_max = sum(1 for x in c.values() if x == f_max)
+        n_max = sum(f==f_max for f in c.values())
         return max(len(tasks), (f_max-1)*(n+1)+n_max)
