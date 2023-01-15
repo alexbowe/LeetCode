8b83274fcca80c9830a658461839ee40da66b783
Alex Bowe <alex@alexbowe.com>
2023-01-14 16:46:20 -0800

    Time: 56 ms (88.94%), Space: 13.8 MB (99.74%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,4 +1,4 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         c = collections.Counter(words)
-        return heapq.nsmallest(k, c, key=lambda w: (-c[w],w))
+        return heapq.nsmallest(k, c.keys(),key=lambda w: (-c[w],w))
