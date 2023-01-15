eaafdf604b5d9bdcc1f325f0019f0b5990f0613d
Alex Bowe <alex@alexbowe.com>
2023-01-14 16:54:03 -0800

    Time: 12593 ms (23.31%), Space: 83.5 MB (6.94%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -6,9 +6,7 @@ class WordDictionary:
     def addWord(self, word: str) -> None:
         if not word: self._root["$"]; return
         self._root[word[0]].addWord(word[1:])
-        self.search.cache_clear()
 
-    @cache
     def search(self, word: str) -> bool:
         if not word: return "$" in self._root
         if word[0] == ".": return any(x.search(word[1:]) for x in self._root.values())
