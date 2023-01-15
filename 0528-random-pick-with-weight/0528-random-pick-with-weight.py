b937cebf0f01c39322951056f67958fc938ead47
Alex Bowe <alex@alexbowe.com>
2023-01-15 14:26:24 -0800

    Time: 405 ms (46.66%), Space: 18.7 MB (41.47%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -3,7 +3,6 @@ class Solution:
     def __init__(self, w: List[int]):
         self._prefix_sum = reduce(lambda xs,x: xs+[xs[-1]+x], w, [0])
         self._prefix_sum = [s/self._prefix_sum[-1] for s in self._prefix_sum]
-        print(self._prefix_sum)
 
     def pickIndex(self) -> int:
         r = random.random()
