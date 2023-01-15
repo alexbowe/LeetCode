c9d9c9658d66082e11da7997caf51a9b2679097e
Alex Bowe <alex@alexbowe.com>
2023-01-14 16:21:45 -0800

    Time: 500 ms (93.03%), Space: 20.1 MB (56.76%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -5,28 +5,27 @@ class Solution:
         def neighbors(r,c):
             if r-1>=0: yield (r-1,c)
             if c-1>=0: yield (r,c-1)
-            if r+1<H:  yield (r+1,c)
-            if c+1<W:  yield (r,c+1)
+            if  r+1<H: yield (r+1,c)
+            if  c+1<W: yield (r,c+1)
         
         START = "*"
-        FOOD  = "#"
-        FREE  = "O"
+        FOOD = "#"
+        FREE = "O"
         OBSTACLE = "X"
         
-        level = [(r,c) for r,c in product(range(H),range(W)) if grid[r][c]==START]
-        seen = set(level)
-        
-        count = 0
+        level = [(r,c) for r,c in product(range(H),range(W)) if grid[r][c] == START]
+        seen = set()
+        length = 0
         while level:
-            next_level = []
+            new_level = []
             for r,c in level:
-                if grid[r][c] == FOOD: return count
+                if grid[r][c] == FOOD: return length
                 for nr,nc in neighbors(r,c):
                     if grid[nr][nc] == OBSTACLE: continue
                     if (nr,nc) in seen: continue
                     seen.add((nr,nc))
-                    next_level.append((nr,nc))
-            level = next_level
-            count+=1
-            
+                    new_level.append((nr,nc))
+            level = new_level
+            length += 1
+        
         return -1
