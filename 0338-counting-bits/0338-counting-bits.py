cdeebcfe41b0b0445ecf0c5ba193cb954ad263d7
Alex Bowe <alex@alexbowe.com>
2023-01-14 16:50:07 -0800

    Time: 79 ms (94.34%), Space: 20.8 MB (26.45%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,5 +2,5 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         result = [0]*(n+1)
         for x in range(1,n+1):
-            result[x] = result[x&(x-1)]+1
+            result[x] = result[x&(x-1)] + 1
         return result
