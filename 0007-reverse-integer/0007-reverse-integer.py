d9c05ce764dc37cacbd8517c308f150e5e3bbde2
Alex Bowe <alex@alexbowe.com>
2023-01-14 16:49:08 -0800

    Time: 39 ms (64.80%), Space: 13.8 MB (97.22%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,10 +1,14 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if not x: return 0
-        MIN,MAX = -2**31, 2**31-1
-        sign, x = x//abs(x), abs(x)
-        result = 0
-        while x:
-            result = 10*result + x%10
-            x//=10
-        return sign*result if MIN<=sign*result<=MAX else 0
+        def digits(x):
+            while x:
+                yield x%10
+                x//=10
+        
+        sign = -1 if x <0 else +1
+        x = abs(x)
+        reverse = 0
+        for d in digits(x):
+            reverse = reverse*10 + d
+        
+        return sign*reverse if -2**31 <= sign*reverse <= 2**31-1 else 0
