f5b575e3113d6275d226a57e4af9aede75f8d0dc
Alex Bowe <alex@alexbowe.com>
2023-01-16 14:16:49 -0800

    Time: 213 ms (87.78%), Space: 18.3 MB (92.49%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,11 +1,11 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self._prefix_sum = list(itertools.accumulate(w))
+        self._w = list(itertools.accumulate(w))
 
     def pickIndex(self) -> int:
-        r = random.randint(1, self._prefix_sum[-1])
-        return bisect.bisect_left(self._prefix_sum, r)
+        r = random.randint(1, self._w[-1])
+        return bisect.bisect_left(self._w, r)
 
 
 # Your Solution object will be instantiated and called as such:
