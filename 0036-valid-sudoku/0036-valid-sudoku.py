7dd355fe7d1afa0cfcaae64a1dd8cfddafc30c2d
Alex Bowe <alex@alexbowe.com>
2023-01-16 14:20:22 -0800

    Time: 128 ms (55.80%), Space: 13.9 MB (80.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,6 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         W = 9
         SW = 3
+        
         row = lambda r: [board[r][c] for c in range(W)]
         col = lambda c: [board[r][c] for r in range(W)]
         sqr = lambda r,c: [board[r*SW+sr][c*SW+sc] for sr,sc in product(range(SW),range(SW))]
@@ -9,9 +10,10 @@ class Solution:
         def is_valid(xs):
             c = collections.Counter(xs)
             del c["."]
-            return not c or max(c.values()) == 1
+            return not c or max(c.values())==1
         
-        valid_rows = all(is_valid(row(r)) for r in range(W))
-        valid_cols = all(is_valid(col(c)) for c in range(W))
-        valid_sqrs = all(is_valid(sqr(r,c)) for r,c in product(range(SW),range(SW)))
-        return valid_rows and valid_cols and valid_sqrs
+        return all([
+            all(is_valid(row(r)) for r in range(W)),
+            all(is_valid(col(c)) for c in range(W)),
+            all(is_valid(sqr(r,c)) for r,c in product(range(SW),range(SW))),
+        ])
