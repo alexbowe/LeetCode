64236fe1bb4309fca930a7e35dbae57d37ac87a5
Alex Bowe <alex@alexbowe.com>
2023-01-26 17:30:59 -0800

    Time: 7068 ms (6.81%), Space: 17.3 MB (92.13%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,12 +5,13 @@ class Solution:
         def twoSum(nums, start, target):
             d = dict()
             for i in range(start, len(nums)):
-                diff = target - nums[i]
-                if diff in d: yield [d[diff], i]
-                else: d[nums[i]] = i
+                x = nums[i]
+                delta = target-x
+                if delta in d: yield (d[delta], i)
+                d[x] = i
         
         result = set()
         for i in range(len(nums)-2):
             for j,k in twoSum(nums, i+1, -nums[i]):
-                result.add((nums[i],nums[j],nums[k]))
+                result.add((nums[i], nums[j], nums[k]))
         return list(result)
