7c5f02edda93fb6ad69dad7f7fe7a15047a8de95
Alex Bowe <alex@alexbowe.com>
2023-02-01 09:27:42 -0800

    Time: 104 ms (66.62%), Space: 15.5 MB (87.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,20 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def make_graph(prerequisites):
-            g = collections.defaultdict(lambda: {"indegree":0, "out": set()})
-            for v,u in prerequisites:
-                g[u]["out"].add(v)
-                g[v]["indegree"]+=1
+        def make_graph(n, edges):
+            g = {v:{"outgoing": set(), "indegree": 0} for v in range(n)}
+            for v,u in edges:
+                g[u]["outgoing"].add(v)
+                g[v]["indegree"] += 1
             return g
         
-        g = make_graph(prerequisites)
-        starts = {v for v,node in g.items() if node["indegree"] == 0}
+        g = make_graph(numCourses, prerequisites)
+        starts = [v for v in g.keys() if g[v]["indegree"]==0]
+        if g and not starts: return False
         
         while starts:
-            u = starts.pop()
-            for v in g[u]["out"]:
-                g[v]["indegree"]-=1
-                if g[v]["indegree"] == 0: starts.add(v)
-            del g[u]
-        
-        return not g
+            new_starts = []
+            for u in starts:
+                for v in g[u]["outgoing"]:
+                    g[v]["indegree"] -= 1
+                    if g[v]["indegree"] == 0: new_starts.append(v)
+                del g[u]
+            starts = new_starts
+            
+        return not bool(g)
