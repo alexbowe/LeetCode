86166a6a9bf4838403c1f20ec12fa1fefe5bd869
Alex Bowe <alex@alexbowe.com>
2023-02-04 15:55:50 -0800

    Time: 121 ms (76.58%), Space: 15.2 MB (27.00%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,20 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        result = []
-        
-        W = len(p)
-        missing = W
+        M = len(p)
         need = collections.Counter(p)
+        missing = M
         
+        result = []
         l = 0
         for r,c in enumerate(s):
             missing -= need[c]>0
             need[c] -= 1
-            if r>=W:
+            if r>=M:
                 need[s[l]] += 1
                 missing += need[s[l]]>0
-                l+=1
-            if missing == 0:
-                result.append(l)
+                l += 1
+            if missing == 0: result.append(l)
         return result
-                
