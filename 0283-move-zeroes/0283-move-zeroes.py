545acf36bb7a5cee22467574d867479e8a7743d5
Alex Bowe <alex@alexbowe.com>
2023-02-09 11:14:01 -0800

    Time: 164 ms (90.02%), Space: 15.7 MB (11.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,5 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         N = len(nums)
-        nums[:] = (x for x in nums if x != 0)
+        nums[:] = (x for x in nums if x!=0)
         nums.extend([0]*(N-len(nums)))
