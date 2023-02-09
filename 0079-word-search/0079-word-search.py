476ca44be833cb5c6c390444a9d2013a5025abb6
Alex Bowe <alex@alexbowe.com>
2023-02-08 18:40:34 -0800

    Time: 8849 ms (21.89%), Space: 14.1 MB (10.60%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,20 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         H,W = len(board), len(board[0])
-        
         def search(pos, i=0, seen=None):
             r,c = pos
-            if i >= len(word): return True
-            if r<0 or c<0 or H<=r or W<=c: return False
-            if (r,c) in (seen := seen or set()): return False
+            seen = seen or set()
+            if i>=len(word): return True
+            if r<0 or c<0 or r>=H or c>=W: return False
             if word[i] != board[r][c]: return False
-            seen.add((r,c))
+            if pos in seen: return False
+            seen.add(pos)
             result = any([
-                search((r+1,c),i+1,seen),
-                search((r-1,c),i+1,seen),
-                search((r,c+1),i+1,seen),
-                search((r,c-1),i+1,seen),
+                search((r-1,c), i+1, seen),
+                search((r,c-1), i+1, seen),
+                search((r+1,c), i+1, seen),
+                search((r,c+1), i+1, seen),
             ])
-            seen.remove((r,c))
+            seen.remove(pos)
             return result
-        
-        return any(search((r,c)) for r,c in product(range(H),range(W)))
+        return any(search((r,c)) for (r,c) in product(range(H),range(W)))
