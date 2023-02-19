e4acc8551b9bd8d0bf14bd48b7980325044f903c
Alex Bowe <alex@alexbowe.com>
2023-02-19 13:37:54 -0800

    Time: 1483 ms (38.66%), Space: 28.7 MB (61.50%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,7 +1,7 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0]*len(temperatures)
         s = []
+        result = [0] * len(temperatures)
         for r,t in enumerate(temperatures):
             while s and temperatures[s[-1]]<t:
                 l = s.pop()
