9fcd0c94f97abee92c178422313799f1829efa15
Alex Bowe <alex@alexbowe.com>
2023-02-23 19:01:12 -0800

    Time: 29 ms (86.24%), Space: 13.8 MB (98.02%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -3,8 +3,7 @@ class Solution:
         def apply_backspace(s):
             result = []
             for x in s:
-                if x == "#": result = result[:-1]
+                if x == "#": result.pop() if result else None
                 else: result.append(x)
             return "".join(result)
-        
         return apply_backspace(s) == apply_backspace(t)
