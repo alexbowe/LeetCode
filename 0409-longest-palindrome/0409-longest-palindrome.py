29626bb8d1d4becc8aab394c49c24af8f15cc718
Alex Bowe <alex@alexbowe.com>
2023-08-31 12:38:22 -0700

    Time: 47 ms (39.03%), Space: 16.4 MB (42.44%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,15 +1,7 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         c = collections.Counter(s)
-
-        """
-        ccc -> 3
-        if odd, a
-        """
-
-        even = 0
-        odd = 0
-        for x,count in c.items():
-            even += count//2
-            odd += count%2
-        return 2*even + (odd>0)
+        even = sum(x//2 for x in c.values())
+        odd = sum(x%2 for x in c.values())
+        return even*2 + (odd>0)
+        
