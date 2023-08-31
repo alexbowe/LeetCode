23c33325bfa9b82a4ec17db67ee0fbe3556904c9
Alex Bowe <alex@alexbowe.com>
2023-08-31 13:58:41 -0700

    Time: 700 ms (77.02%), Space: 78.3 MB (48.61%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -4,46 +4,48 @@ class ListNode:
         self.val = val
         self.prev = None
         self.next = None
-    
-    def remove(self):
-        if self.prev: self.prev.next = self.next
-        if self.next: self.next.prev = self.prev
-        self.prev = None
-        self.next = None
-        return self
-    
+
     def post_insert(self, other):
         other.prev = self
         other.next = self.next
         if self.next: self.next.prev = other
         self.next = other
+        return other
+    
+    def disconnect(self):
+        if self.prev: self.prev.next = self.next 
+        if self.next: self.next.prev = self.prev
+        self.prev = None
+        self.next = None
         return self
 
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.d = {}
-        self.list_head = ListNode()
-        self.list_tail = ListNode()
-        self.list_head.post_insert(self.list_tail)
 
-    def get(self, key: int) -> int:
-        if key not in self.d: return -1
-        node = self.d[key]
-        node.remove()
-        self.list_head.post_insert(node)
-        return node.val
+    def __init__(self, capacity: int):        
+        self._d = dict()
+        self._capacity = capacity
+        self._list_head = ListNode()
+        self._list_tail = ListNode()
+        self._list_head.post_insert(self._list_tail)
     
-    def pop(self):
-        if not self.d: return # Raise exception instead
-        return self.d.pop(self.list_tail.prev.remove().key)
+    def get(self, key: int) -> int:
+        if key not in self._d: return -1
+        return self._list_head.post_insert(self._d[key].disconnect()).val
+    
+    def _pop_right(self):
+        del self._d[self._list_tail.prev.disconnect().key]
 
     def put(self, key: int, value: int) -> None:
-        if self.get(key) != -1: self.d[key].val = value; return
-        if len(self.d) == self.capacity: self.pop()
-        self.d[key] = ListNode(key=key,val=value)
-        self.list_head.post_insert(self.d[key])
+        if self.get(key) != -1:
+            self._d[key].val = value
+            return
         
+        if len(self._d) == self._capacity:
+            self._pop_right()
+        
+        self._d[key] = self._list_head.post_insert(ListNode(key=key, val=value))
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
