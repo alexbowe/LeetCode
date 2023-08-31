7896a1ffa95f58cce350f89804789ae58ac462d0
Alex Bowe <alex@alexbowe.com>
2023-08-31 13:02:32 -0700

    Time: 43 ms (60.7%), Space: 16.6 MB (42.72%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums: return []
         result = [[]]
         for x in nums:
-            result += [r+[x] for r in result]
+            result += [r + [x] for r in result]
         return result
