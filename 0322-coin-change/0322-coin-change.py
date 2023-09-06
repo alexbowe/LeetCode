11aef8e3364852071226ac8010380c6f11c54aaa
Alex Bowe <alex@alexbowe.com>
2023-09-05 18:07:11 -0700

    Time: 485 ms (99%), Space: 17.5 MB (28.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        totals = {0}
-        seen = set()
         n = 0
-        while totals:
-            if amount in totals: return n
-            totals = {x+c for c in coins for x in totals if x+c <= amount and x+c not in seen}
-            seen.update(totals)
-            n += 1
+        level = {0}
+        seen = {0}
+        while level:
+            if amount in level: return n
+            n+=1
+            level = {
+                v+c for v in level
+                for c in coins
+                if v+c <= amount
+                and v+c not in seen
+            }
+            seen.update(level)
         return -1
+            
