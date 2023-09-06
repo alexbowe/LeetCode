d1912be745346086ecf071d01d44e091592c7417
Alex Bowe <alex@alexbowe.com>
2023-09-05 18:47:08 -0700

    Time: 81 ms (78.76%), Space: 19.8 MB (42.02%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,7 +4,6 @@ class Solution:
             return y[0] <= x[1]
 
         def merge(xs, y):
-            print(xs, y)
             if not xs: return [y]
             x = xs.pop()
             return xs + [[min(x[0],y[0]), max(x[1],y[1])]] if overlap(x,y) else xs + [x,y]
