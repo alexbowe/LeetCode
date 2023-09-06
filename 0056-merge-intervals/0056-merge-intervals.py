297eceb98838ce7560f6c7da71a10ad8bafa4ac2
Alex Bowe <alex@alexbowe.com>
2023-09-05 19:09:37 -0700

    Time: 246 ms (5.31%), Space: 21.1 MB (55.8%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,6 +1,7 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=tuple)
+        
         def overlap(x,y):
             return y[0] <= x[1]
         
