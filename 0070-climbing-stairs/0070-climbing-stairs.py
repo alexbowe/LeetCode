068d51a7ca1b6b83631ac2d185a8b8ada6379d88
Alex Bowe <alex@alexbowe.com>
2023-09-05 19:14:01 -0700

    Time: 29 ms (96.78%), Space: 16.4 MB (27.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,27 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        """
-        n = 0:
-        1
-        
-        n = 1:
-        1
-        
-        n = 2:
-        2
-        
-        n = 3:
-        3
-        
-        n = 4:
-        5
-        1 + 1 + 1 + 1
-        1 + 1 + 2
-        1 + 2 + 1
-        2 + 1 + 1
-        2 + 2
-        """
-        a,b = 1, 1
+        a,b = 0,1
         for _ in range(n):
             a,b = b,a+b
-        return a
+        return b
