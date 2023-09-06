22b9a3b2c4c6cbefcf4d8b6069fd1893d5aa1b00
Alex Bowe <alex@alexbowe.com>
2023-09-05 19:09:36 -0700

    Time: 236 ms (5.31%), Space: 21.2 MB (55.8%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,20 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=tuple)
+        intervals.sort(key=tuple)
+        def overlap(x,y):
+            return y[0] <= x[1]
         
-        def overlaps(a, b):
-            return b[0] <= a[1]
+        def merge(x,y):
+            if not overlap(x,y): return [x,y]
+            return [[min(x[0],y[0]), max(x[1],y[1])]]
         
-        def merge_intervals(a, b):
-            if not overlaps(a,b):
-                return [a, b]
-            
-            return [[min(a[0], b[0]), max(a[1],b[1])]]
+        def reducer(xs, y):
+            if not xs: return [y]
+            return xs + merge(xs.pop(), y)
         
-        def reducer(xs, x):
-            if not xs: return [x]
-            prev = xs.pop()
-            return xs + merge_intervals(prev, x)
-                
         return reduce(reducer, intervals, [])
-        
