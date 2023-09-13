815685feed755db379a533144bcf674c84fe9807
Alex Bowe <alex@alexbowe.com>
2023-09-13 14:44:42 -0700

    Time: 62 ms (62.57%), Space: 16.3 MB (81.85%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def helper(partial, options, t):
-            if t < 0: return
-            if t == 0: yield partial
-            for i in range(len(options)):
-                yield from helper(partial+[options[i]], options[i:], t-options[i])
+        def helper(partial, opts, rem):
+            if rem < 0: return
+            if rem == 0: yield partial; return
+            for i in range(len(opts)):
+                # For combinations, replacement is allowed and order doesn't matter.
+                # Using opts[i:] forces the DFS to expand in order of candidates so
+                # we do not need a visited set. If the candidates had repeats, we could
+                # store solutions as indexes into candidates instead.
+                yield from helper(partial + [opts[i]], opts[i:], rem-opts[i])
         return list(helper([], candidates, target))
