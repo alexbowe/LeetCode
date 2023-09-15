fede08ea009f72240cd47e06c48ccb14741fc8f1
Alex Bowe <alex@alexbowe.com>
2023-09-15 16:27:45 -0700

    Time: 39 ms (77.13%), Space: 16.2 MB (72.29%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,8 +1,7 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         MIN,MAX = -2**31, 2**31 - 1
-        sign = +1
-        num = 0
+        sign, num = +1, 0
         for i,x in enumerate(s.lstrip()):
             if i==0 and x=="-": sign = -1
             elif i==0 and x=="+": continue
