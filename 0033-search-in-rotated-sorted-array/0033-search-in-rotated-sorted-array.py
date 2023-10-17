68d5969ca7a5e505c455cd6206270ced031f70da
Alex Bowe <alex@alexbowe.com>
2023-10-17 15:06:06 -0700

    Time: 54 ms (16.04%), Space: 16.7 MB (52.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        pivot = bisect.bisect_left(nums, True, key=lambda x:x<=nums[-1])
+        pivot = bisect.bisect_left(nums, True, key=lambda x: x<=nums[-1])
         left  = bisect.bisect_left(nums, target, hi=pivot)
         right = bisect.bisect_left(nums, target, lo=pivot)
-        
-        if  left < len(nums) and  nums[left] == target: return left
+
+        if  left < len(nums) and nums[left]  == target: return left
         if right < len(nums) and nums[right] == target: return right
         return -1
