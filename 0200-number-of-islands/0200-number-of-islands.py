1c0cbcd52e05234e2626bb6ff5ec7a0d7d516ac2
Alex Bowe <alex@alexbowe.com>
2023-10-20 23:39:42 -0700

    Time: 382 ms (6.08%), Space: 30.8 MB (5.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,17 +4,16 @@ class UnionFind:
         self._parent = dict()
     
     def find(self, x):
-        if self._parent.setdefault(x, x) == x: return x
+        if x == self._parent.setdefault(x,x): return x
         self._parent[x] = self.find(self._parent[x])
         return self._parent[x]
-
+    
     def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        x,y = sorted([x,y], key=self._rank.__getitem__)
+        x, y = self.find(x), self.find(y)
+        x, y = sorted([x, y], key=self._rank.__getitem__)
         self._parent[y] = x
         self._rank[x] += self._rank[x] == self._rank[y]
-
+    
     def count(self):
         return sum(x==p for x,p in self._parent.items())
 
@@ -25,14 +24,14 @@ class Solution:
         def neighbors(r,c):
             if 0<=r-1: yield (r-1,c)
             if 0<=c-1: yield (r,c-1)
-
+        
         uf = UnionFind()
 
-        for r,c in product(range(H), range(W)):
+        for r,c in product(range(H),range(W)):
             if grid[r][c] != "1": continue
             uf.find((r,c))
-            for nr, nc in neighbors(r,c):
+            for nr,nc in neighbors(r,c):
                 if grid[nr][nc] != "1": continue
-                uf.union((nr,nc),(r,c))
+                uf.union((r,c), (nr,nc))
         
         return uf.count()
