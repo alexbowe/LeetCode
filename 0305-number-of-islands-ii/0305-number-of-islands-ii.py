e8473259cd0ccd7bce4c159aa23e8da8fddd63c4
Alex Bowe <alex@alexbowe.com>
2024-01-16 12:18:42 -0800

    Update 0305-number-of-islands-ii.py

--- 0305-number-of-islands-ii/0305-number-of-islands-ii.py
+++ 0305-number-of-islands-ii/0305-number-of-islands-ii.py
@@ -1,42 +1,46 @@
 class UnionFind:
     def __init__(self):
-        self._rank   = collections.defaultdict(int)
         self._parent = dict()
-        self._count  = 0
+        self._rank = collections.defaultdict(int)
+        self._count = 0
+
+    def __contains__(self, x):
+        return x in self._parent
     
     def find(self, x):
-        if x not in self._parent: self._count += 1
-        if x == self._parent.setdefault(x, x): return x
+        if x not in self._parent:
+            self._parent[x] = x
+            self._count += 1
+        if x == self._parent[x]: return x
         self._parent[x] = self.find(self._parent[x])
         return self._parent[x]
     
     def union(self, x, y):
-        x, y = self.find(x), self.find(y)
-        if x != y: self._count -= 1
-        x, y = sorted([x,y], key=self._rank.__getitem__)
-        self._parent[y] = x
-        self._rank[x] += self._rank[x] == self._rank[y]
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+        x,y = sorted([x,y], key=self._rank.__getitem__)
+        self._parent[x] = y
+        self._rank[y] += self._rank[x] == self._rank[y]
+        self._count -= 1
     
     def count(self):
         return self._count
 
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        def neighbors(r,c):
-            if 0<=r-1: yield (r-1,c)
-            if 0<=c-1: yield (r,c-1)
-            if  r+1<m: yield (r+1,c)
-            if  c+1<n: yield (r,c+1)
-        
-        s = set()
         uf = UnionFind()
-        result = []
 
+        def neighbors(r,c):
+            yield r-1,c
+            yield r+1,c
+            yield r,c-1
+            yield r,c+1
+
+        ans = []
         for r,c in positions:
             uf.find((r,c))
             for nr,nc in neighbors(r,c):
-                if (nr,nc) in s: uf.union((nr,nc),(r,c))
-            s.add((r,c))
-            result.append(uf.count())
-        
-        return result
+                if (nr,nc) in uf: uf.union((r,c),(nr,nc))
+            ans.append(uf.count())
+        return ans
