b1765a87d16624602cb69d837325642f74c3c4fd
Alex Bowe <alex@alexbowe.com>
2024-01-17 11:14:28 -0800

    Explain 1235-maximum-profit-in-job-scheduling.py

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,9 +1,16 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         N = len(profit)
-        start, end, profit = zip(*sorted(zip(startTime, endTime, profit)))
-        skip = [bisect.bisect_left(start, end[i]) for i in range(N)]
+        start, end, profit = zip(*sorted(zip(startTime,endTime,profit)))
+        next_available_job = [bisect.bisect_left(start,end[i]) for i in range(N)]
+
+        # If we started working at the last job only, that would be the max
+        # profit for that job. With this insight, we can work backwards
+        # to find whether or not to do each job.
+        # N+1 because the very least profit is obtained by not doing any jobs.
         dp = [0] * (N+1)
         for i in reversed(range(N)):
-            dp[i] = max(dp[i+1], profit[i] + dp[skip[i]])
+            # We either skip this job (dp[i+1]) or do it and add its
+            # profit to the profit for the next possible job slot.
+            dp[i] = max(dp[i+1], profit[i] + dp[next_available_job[i]])
         return dp[0]
