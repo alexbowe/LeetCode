a64b738fcc768d1e81b570c2d0a9be6561083277
Alex Bowe <alex@alexbowe.com>
2024-01-17 10:05:26 -0800

    Update 0224-basic-calculator.py

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,20 +1,33 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        result, sign, num = 0, +1, 0
+        s = s.strip()
+
         stack = []
+        sign, result, num = +1, 0, 0
+
         for i,x in enumerate(s):
-            if x.isdigit(): num = 10*num + int(x)
-            if i == len(s)-1 or x in "+-)":
+            # Accumulate digits to find a number
+            if x.isdigit(): num = num*10 + int(x)
+
+            # When a number ends, add it to the result
+            if i == len(s)-1 or x in "-+)":
                 result += sign*num
                 sign, num = +1, 0
-            
-            if x == "-": sign = -1
-            if x == "+": sign = +1
-            if x == "(":
-                stack.extend([result, sign])
+
+            # Is the number positive or negative?
+            if   x == "+": sign = +1
+            elif x == "-": sign = -1
+
+            # If we are starting a new sub-expression
+            # We need a new sub-result and sign
+            # (num is not needed as we don't allow numbers
+            # before parentheses, so it will always be 0)
+            elif x == "(":
+                stack.append((sign, result))
                 sign, result = +1, 0
-            if x == ")":
-                sign, prev_result = stack.pop(), stack.pop()
-                result = prev_result + sign*result
-                sign = +1
+            elif x == ")":
+                prev_sign, prev_result = stack.pop()
+                result = prev_result + prev_sign*result
+                sign, num = +1, 0
+
         return result
