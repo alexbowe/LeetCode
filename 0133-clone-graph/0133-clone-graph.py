819e8bca8791e293ae95e579f5ee9d18130cc4d9
Alex Bowe <alex@alexbowe.com>
2024-01-16 19:08:52 -0800

    Update 0133-clone-graph.py

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -12,9 +12,9 @@ class Solution:
             if not curr: return None
             nodes = nodes or dict()
             if curr.val in nodes: return nodes[curr.val]
-
             new_node = nodes.setdefault(curr.val, Node(curr.val))
             for n in curr.neighbors:
                 new_node.neighbors.append(helper(n, nodes))
             return new_node
+        
         return helper(node)
