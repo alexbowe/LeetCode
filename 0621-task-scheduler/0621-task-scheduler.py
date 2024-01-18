299da2b695ab820f5efe35773bfff16e61030683
Alex Bowe <alex@alexbowe.com>
2024-01-18 10:59:59 -0800

    Update 0621-task-scheduler.py

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,6 +1,7 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        c = collections.Counter(tasks)
-        f_max = max(c.values())
-        n_max = sum(f==f_max for f in c.values())
-        return max(len(tasks), (f_max-1)*(n+1)+n_max)
+        # https://medium.com/@satyem77/task-scheduler-leetcode-39d579f3440
+        freqs = list(collections.Counter(tasks).values())
+        f_max = max(freqs)
+        n_max = freqs.count(f_max)
+        return max(len(tasks), (n+1)*(f_max-1)+n_max)
