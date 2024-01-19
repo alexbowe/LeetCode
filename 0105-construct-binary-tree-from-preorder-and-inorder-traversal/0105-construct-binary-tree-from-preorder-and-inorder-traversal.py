3a4f85be12282ca5628b69c9f13fca18b94f3d53
Alex Bowe <alex@alexbowe.com>
2024-01-19 11:37:00 -0800

    Update 0105-construct-binary-tree-from-preorder-and-inorder-traversal.py

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,11 +6,19 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        """
+        Observations:
+        - preorder = [root][preorder_left][preorder_right]
+        - inorder  = [inorder_left][root][inorder_right]
+        - Dont know value of root from inorder, but preorder tells us that.
+        - Dont know size of preorder_left or right, but inorder tells us that.
+        """
+        assert len(preorder) == len(inorder)
         if not preorder or not inorder: return None
-        x = preorder[0]
-        i = inorder.index(x)
+        root_val = preorder[0]
+        root_pos = inorder.index(root_val)        
         return TreeNode(
-            val   = x,
-            left  = self.buildTree(preorder[1:i+1], inorder[:i+1]),
-            right = self.buildTree(preorder[i+1:], inorder[i+1:]),
+            val = root_val,
+            left = self.buildTree(preorder[1:1+root_pos],inorder[:root_pos]),
+            right = self.buildTree(preorder[1+root_pos:],inorder[root_pos+1:]),
         )
