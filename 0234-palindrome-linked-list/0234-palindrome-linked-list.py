2affc87f1052389374cdac5216e7ca930530bffd
Alex Bowe <alex@alexbowe.com>
2024-06-17 10:01:57 -0700

    Time: 297 ms (45.27%), Space: 32.1 MB (69.19%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,29 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-def reverse(curr):
-    prev = None
-    while curr:
-        temp = curr.next
-        curr.next = prev
-        prev = curr
-        curr = temp
-    return prev
-
-def iterlist(xs):
-    while xs:
-        yield xs.val
-        xs = xs.next
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        left  = iterlist(head)
-        right = iterlist(reverse(slow))
-        
-        return all(x==y for x,y in zip(left, right))
+      """
+          
+              f
+      1 2 5 2 1
+          s
+      """
+      def iter_list(curr):
+        while curr:
+          yield curr
+          curr = curr.next
+
+      def reverse_list(curr):
+        prev = None
+        while curr:
+          temp = curr.next
+          curr.next = prev
+          prev = curr
+          curr = temp
+        return prev      
+      
+      fast = slow = head
+      while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+      
+      # Reverse slow (which is second half of list)
+      tail = reverse_list(slow)
+
+      for x,y in zip(iter_list(head), iter_list(tail)):
+        if x.val != y.val: return False
+      
+      return True
