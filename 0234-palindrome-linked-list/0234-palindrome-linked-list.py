586080eae780f3cdcf123b7c0ea68d183127f34f
Alex Bowe <alex@alexbowe.com>
2024-06-23 12:39:59 -0700

    Time: 280 ms (80.9%), Space: 31.8 MB (94.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,25 +5,38 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-      def iter_list(curr):
-        while curr:
-          yield curr
-          curr = curr.next
+        """
+            s
+        1 2 2 1
+                f
+        """
 
-      def reverse_list(curr):
-        prev = None
-        while curr:
-          temp = curr.next
-          curr.next = prev
-          prev = curr
-          curr = temp
-        return prev      
-      
-      fast = slow = head
-      while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-      
-      tail = reverse_list(slow)
+        def iter_list(curr):
+            while curr:
+                yield curr
+                curr = curr.next
 
-      return all(x.val==y.val for x,y in zip(iter_list(head), iter_list(tail)))
+        def reverse(curr):
+            prev = None
+            while curr:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            return prev
+        
+        # Find middle
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse middle
+        left, right = head, reverse(slow)
+
+        # Match
+        return all(
+            a.val==b.val
+            for a,b
+            in zip(iter_list(left), iter_list(right))
+        )
