b73161b7355c3ccf0f7534a2640ac735e63ea2d6
Alex Bowe <alex@alexbowe.com>
2024-07-06 16:44:14 -0700

    Time: 46 ms (61.86%), Space: 119.7 MB (5.67%) - LeetHub

---
+++ 0604-design-compressed-string-iterator/0604-design-compressed-string-iterator.py
@@ -0,0 +1,31 @@
+def decode(s):
+    count = 0
+    char = ""
+    for x in s+".":
+        if x.isdigit():
+            count = 10*count+int(x)
+        else:
+            yield from char*count
+            count = 0
+            char = x
+
+class StringIterator:
+
+    def __init__(self, compressedString: str):
+        self._it = decode(compressedString)
+        self._next = next(self._it, None)
+
+    def next(self) -> str:
+        if not self.hasNext(): return " "
+        x, self._next = self._next, next(self._it, None)
+        return x
+
+    def hasNext(self) -> bool:
+        return self._next is not None
+        
+
+
+# Your StringIterator object will be instantiated and called as such:
+# obj = StringIterator(compressedString)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
