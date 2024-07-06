e0ab478a544f8e7ad02bd3ef17bcfb8430ef8949
Alex Bowe <alex@alexbowe.com>
2024-07-06 14:59:14 -0700

    Time: 3797 ms (23.28%), Space: 24.4 MB (9.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,19 +1,38 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        """
+        j<i -> False
+        i==j -> True
+        i==j+1 and s[i]==s[j] -> True
+        s[i]==s[j] and P(i+1,j-1) -> True
+
+          babad
+        b 10100
+        a  1010
+        b   100
+        a    10
+        d     1
+    
+        """
+
         N = len(s)
         P = [[0]*N for _ in range(N)]
-        
+
         for i in range(N):
-            P[i][i] = True
-            if i+1 < N: P[i][i+1] = s[i] == s[i+1]
-        
-        for i in reversed(range(N)):
+            P[i][i] = 1
+
+        for i in range(N-1):
+            P[i][i+1] = int(s[i] == s[i+1])
+
+        for i in range(N-2,-1,-1):
             for j in range(i+2,N):
-                P[i][j] = P[i+1][j-1] and s[i] == s[j]
-        
-        L = R = 0
+                P[i][j] = P[i+1][j-1] * int(s[i]==s[j])
+
+        # print("\n".join("".join(str(x) for x in row) for row in P))
+
+        I,J = 0,0
         for i in range(N):
-            for j in range(i,N):
-                if P[i][j] and j-i > R-L: L,R = i,j
+            for j in range(i, N):
+                if P[i][j]==1 and j-i>J-I: I,J = i,j
         
-        return s[L:R+1]
+        return s[I:J+1]
