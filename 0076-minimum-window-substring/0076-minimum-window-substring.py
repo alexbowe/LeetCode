f11b3c0d11dce50d325cbc5c7b3d1d77056f7dfb
Alex Bowe <alex@alexbowe.com>
2024-07-06 17:00:07 -0700

    Time: 121 ms (54.83%), Space: 17.2 MB (57.27%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,12 +1,12 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need = collections.Counter(t)
+        need = Counter(t)
         missing = len(t)
-        l = L = R = 0
-        for r,c in enumerate(s,1):
-            missing -= need[c]>0
-            need[c] -= 1
-            if missing>0: continue
+        l,L,R = 0,0,0
+        for r,x in enumerate(s,1):
+            missing -= need[x]>0
+            need[x]-=1
+            if missing > 0: continue
             while need[s[l]]<0:
                 need[s[l]]+=1
                 l+=1
