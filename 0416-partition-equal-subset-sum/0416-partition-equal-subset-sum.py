4e6b67ccff61b4fb53281a31a78d7f4f262475d2
Alex Bowe <alex@alexbowe.com>
2024-07-12 09:46:12 -0700

    Time: 841 ms (43.11%), Space: 21 MB (49.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,4 +1,4 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        subtotals = reduce(lambda xs,x:xs|{s+x for s in xs}, nums, {0})
-        return sum(nums)/2 in subtotals
+        sums = reduce(lambda xs,x: xs|{s+x for s in xs}, nums, {0})
+        return sum(nums)/2.0 in sums
