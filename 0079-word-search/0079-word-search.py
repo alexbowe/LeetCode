207af580af74a474b244081ea8ab59dae80e7884
Alex Bowe <alex@alexbowe.com>
2024-11-08 02:01:08 -0800

    Time: 8 ms (98.49%), Space: 16.8 MB (20.22%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,20 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         H,W = len(board), len(board[0])
-        def search(pos, i=0, seen=None):
-            r,c = pos
+
+        if len(word) > H*W: return False
+
+        board_count = collections.Counter(sum(board, []))
+        word_count = collections.Counter(word)
+        if not word_count <= board_count: return False
+
+        if board_count[word[0]] > board_count[word[-1]]: word = word[::-1]
+
+        def find(w, row, col, seen = None):
+            if not w: return True
+            if row < 0 or row >= H: return False
+            if col < 0 or col >= W: return False
+            if board[row][col] != w[0]: return False
+            
             seen = seen or set()
-            if i>=len(word): return True
-            if r<0 or c<0 or r>=H or c>=W: return False
-            if word[i] != board[r][c]: return False
-            if pos in seen: return False
-            seen.add(pos)
+            if (row,col) in seen: return False
+            
+            seen.add((row,col))
             result = any([
-                search((r-1,c), i+1, seen),
-                search((r,c-1), i+1, seen),
-                search((r+1,c), i+1, seen),
-                search((r,c+1), i+1, seen),
+                find(w[1:], row-1,   col, seen),
+                find(w[1:], row+1,   col, seen),
+                find(w[1:],   row, col-1, seen),
+                find(w[1:],   row, col+1, seen),
             ])
-            seen.remove(pos)
+            seen.remove((row,col))
             return result
-        return any(search((r,c)) for (r,c) in product(range(H),range(W)))
+        return any(find(word,r,c) for r,c in product(range(H),range(W)))
